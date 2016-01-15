ea7a3d9774ae22a06ba05da22cfb99a13c529aa4
Édouard Lopez <dev+git@edouard-lopez.com>
2016-01-15 11:35:13 +0100

    fetch_source

--- installer.fish
+++ installer.fish
@@ -3,11 +3,20 @@
 function install
     set -l fish_function_path (echo $HOME/.config/fish/functions) $fish_function_path
     check_git_is_present
+    fetch_source
 end
 
 function check_git_is_present
     command --search git >/dev/null 2>&1; or begin;
         printf "Error: git is not installed\n"
+        exit 1
+    end
+end
+
+function fetch_source
+    printf "Fetching theme's source\n"
+    env git clone --depth=1 https://github.com/rafaelrinaldi/theme-pure.git; or begin;
+        printf "Error: git clone of theme-pure repo failed\n"
         exit 1
     end
 end
