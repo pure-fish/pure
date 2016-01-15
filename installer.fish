bafd7368a0b7a8c438899cc1ebcda100ad2a03cc
Édouard Lopez <dev+git@edouard-lopez.com>
2016-01-15 11:34:42 +0100

    add check_git_is_present

--- installer.fish
+++ installer.fish
@@ -2,6 +2,14 @@
 
 function install
     set -l fish_function_path (echo $HOME/.config/fish/functions) $fish_function_path
+    check_git_is_present
+end
+
+function check_git_is_present
+    command --search git >/dev/null 2>&1; or begin;
+        printf "Error: git is not installed\n"
+        exit 1
+    end
 end
 
 install
