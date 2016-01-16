3f5d826400acd2e0b4b7ce34ab1e76eb118db19e
Itzik Ephraim <oranja@gmail.com>
2016-01-16 14:03:13 +0200

    A complete Fish installer for pure

--- installer.fish
+++ installer.fish
@@ -1,34 +1,45 @@
-#!/usr/bin/env fish
+function install_pure
+    test (count $argv) -ge 1;
+        and set FISH_CONFIG_DIR $argv[1];
+        or set FISH_CONFIG_DIR "$HOME/.config/fish"
 
-function install
-    check_git_is_present
-    fetch_source
-    enable_theme
-end
+    test (count $argv) -ge 2;
+        and set PURE_INSTALL_DIR $argv[2];
+        or set PURE_INSTALL_DIR "$FISH_CONFIG_DIR/functions/theme-pure"
 
-function check_git_is_present
+    # Check if 'git' is available
     command --search git >/dev/null 2>&1; or begin;
         printf "Error: git is not installed\n"
         exit 1
     end
-end
 
-function fetch_source
+    # Fetch source
     printf "Fetching theme's source\n"
-    env git clone --depth=1 https://github.com/rafaelrinaldi/theme-pure.git; or begin;
+    env git clone --depth=1 https://github.com/rafaelrinaldi/theme-pure.git $PURE_INSTALL_DIR; or begin;
         printf "Error: git clone of theme-pure repo failed\n"
         exit 1
     end
-end
 
-function enable_theme
+    # Install theme
+    echo "Installing theme"
+
+    #  Ignore previous theme
+    set -l old_prompt $FISH_CONFIG_DIR/functions/fish_prompt.fish
+    if test -f $old_prompt
+        mv $old_prompt $old_prompt.ignore
+        echo "Saved previous fish_prompt.fish to $old_prompt.ignore"
+    end
+
+    #  Enable autoloading for pure's functions on shell init
+    set -l marker "# THEME PURE #"
+    if not test (grep "$THEME_PURE" $FISH_CONFIG_DIR/config.fish 2>&1 >/dev/null)
+        echo "$marker" >> $FISH_CONFIG_DIR/config.fish
+        echo "set fish_function_path $PURE_INSTALL_DIR" '$fish_function_path' >> $FISH_CONFIG_DIR/config.fish
+    end
+    ln -sf $PURE_INSTALL_DIR/fish_prompt.fish $FISH_CONFIG_DIR/functions/
+
+    # Enable theme
     printf "Enabling theme\n"
-    set -l fish_function_path (echo $HOME/.config/fish/functions) $fish_function_path
-    source theme-pure/__format_time.fish
-    source theme-pure/__parse_current_folder.fish
-    source theme-pure/__parse_git_branch.fish
-    source theme-pure/fish_prompt.fish
-    echo "pom"
+    set fish_function_path $PURE_INSTALL_DIR $fish_function_path
+    source $FISH_CONFIG_DIR/functions/fish_prompt.fish
 end
-
-install
