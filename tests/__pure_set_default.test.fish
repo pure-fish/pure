7c13ce332fc2a32aec8ad6f7f06493a7aa92835a
Edouard Lopez <contact@edouard-lopez.com>
2017-12-21 16:20:47 +0100

    test `__pure_set_default`

---
+++ tests/__pure_set_default.test.fish
@@ -0,0 +1,16 @@
+source $DIRNAME/../__pure_set_default.fish
+
+test "set my_var default value"
+  'default_value' = (
+    __pure_set_default my_var 'default_value'
+    echo $my_var
+  )
+end
+
+test "skip setting value if default already exists"
+  'default_value' = (
+    __pure_set_default my_var 'default_value'
+    __pure_set_default my_var 'another_value'
+    echo $my_var
+  )
+end
