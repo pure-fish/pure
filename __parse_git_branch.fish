b921d00d4c81ca9bc48a8b3419396567dfeab56d
William Myers <mwilliammyers@gmail.com>
2018-01-13 18:34:10 -0700

    Simplify setting git_status
    Simplify __parse_git_branch by using `echo` instead of `string`

--- __parse_git_branch.fish
+++ __parse_git_branch.fish
@@ -1,4 +1,4 @@
 function __parse_git_branch -d "Parse current Git branch name"
   command git symbolic-ref --short HEAD ^/dev/null;
-    or string split '\n' (command git show-ref --head -s --abbrev)[1]
+    or echo (command git show-ref --head -s --abbrev HEAD)[1]
 end
