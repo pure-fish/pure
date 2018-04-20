eddd3838541f08945bcf65404ebe1739beb5f0a7
Boye Borg <boye.borg@icloud.com>
2018-04-20 11:41:29 +0200

    give every prompt attribute a configurable color variable

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -25,11 +25,21 @@ __pure_set_default pure_color_blue (set_color blue)
 __pure_set_default pure_color_yellow (set_color yellow)
 __pure_set_default pure_color_cyan (set_color cyan)
 __pure_set_default pure_color_gray (set_color 93A1A1)
+__pure_set_default pure_color_white (set_color white)
 __pure_set_default pure_color_normal (set_color normal)
 
-__pure_set_default pure_username_color $pure_color_gray
-__pure_set_default pure_host_color $pure_color_gray
-__pure_set_default pure_root_color $pure_color_normal
+__pure_set_default pure_color_success $pure_color_magenta
+__pure_set_default pure_color_error $pure_color_red
+__pure_set_default pure_color_folder $pure_color_blue
+__pure_set_default pure_color_duration $pure_color_yellow
+__pure_set_default pure_color_git_dirty $pure_color_gray
+__pure_set_default pure_color_git_branch $pure_color_gray
+__pure_set_default pure_color_git_arrow $pure_color_cyan
+__pure_set_default pure_color_virtualenv $pure_color_gray
+__pure_set_default pure_color_ssh_username $pure_color_gray
+__pure_set_default pure_color_ssh_host $pure_color_gray
+__pure_set_default pure_color_ssh_root $pure_color_white
+__pure_set_default pure_color_ssh_host_user_seperator $pure_color_gray
 
 # Determines whether the username and host are shown at the begining or end
 # 0 - end of prompt, default
@@ -67,13 +77,13 @@ function pre_prompt --on-event fish_prompt
     set -l user (whoami)
 
     if [ "$user" = "root" ]
-      set user "$pure_root_color$user"
+      set user "$pure_color_ssh_root$user"
     else
-      set user "$pure_username_color$user"
+      set user "$pure_color_ssh_username$user"
     end
 
     # Format user and host part of prompt
-    set user_and_host "$user$pure_color_gray@$pure_host_color$host$pure_color_normal "
+    set user_and_host "$user$pure_color_ssh_host_user_seperator@$pure_color_ssh_host$host$pure_color_normal "
   end
 
   if test $pure_user_host_location -eq 1
@@ -81,7 +91,7 @@ function pre_prompt --on-event fish_prompt
   end
 
   # Format current folder on prompt output
-  set pre_prompt $pre_prompt "$pure_color_blue$current_folder$pure_color_normal "
+  set pre_prompt $pre_prompt "$pure_color_folder$current_folder$pure_color_normal "
 
   # Exit with code 1 if git is not available
   if not type -fq git
@@ -119,8 +129,17 @@ function pre_prompt --on-event fish_prompt
       end
     end
 
+    # Add color to the branch name
+    set git_branch_name "$pure_color_git_branch$git_branch_name"
+
+    # Add color to the dirty symbol
+    set git_dirty "$pure_color_git_dirty$git_dirty"
+
+    # Add color to the git arrow
+    set git_arrows "$pure_color_git_arrow$git_arrows"
+
     # Format Git prompt output
-    set pre_prompt $pre_prompt "$pure_color_gray$git_branch_name$git_dirty$pure_color_normal$pure_color_cyan$git_arrows$pure_color_normal "
+    set pre_prompt $pre_prompt "$git_branch_name$git_dirty$git_arrows$pure_color_normal "
   end
 
   if test $pure_user_host_location -ne 1
@@ -132,7 +151,7 @@ function pre_prompt --on-event fish_prompt
     set command_duration (__format_time $CMD_DURATION $pure_command_max_exec_time)
   end
 
-  set pre_prompt $pre_prompt "$pure_color_yellow$command_duration$pure_color_normal"
+  set pre_prompt $pre_prompt "$pure_color_duration$command_duration$pure_color_normal"
 
   echo -e -s $pre_prompt
 end
@@ -143,21 +162,21 @@ function fish_prompt
   # Save previous exit code
   set -l exit_code $status
 
-  # Set default color symbol to green meaning it's all good!
-  set -l color_symbol $pure_color_magenta
+  # Set default symbol color to the success color, meaning it's all good!
+  set -l color_symbol $pure_color_success
 
   # Handle previous failed command
   if test $exit_code -ne 0
-    # Symbol color is red when previous command fails
-    set color_symbol $pure_color_red
+    # Set the symbol color to the error color when the previous command fails
+    set color_symbol $pure_color_error
     if test $pure_separate_prompt_on_error -eq 1
-      set color_symbol $pure_color_red$pure_symbol_prompt$pure_color_green
+      set color_symbol $pure_color_error$pure_symbol_prompt$pure_color_success
     end
   end
 
   # Show python virtualenv name (if activated)
   if test -n "$VIRTUAL_ENV"
-    set prompt $prompt $pure_color_gray(basename "$VIRTUAL_ENV")"$pure_color_normal "
+    set prompt $prompt $pure_color_virtualenv(basename "$VIRTUAL_ENV")"$pure_color_normal "
   end
 
   # vi-mode indicator
