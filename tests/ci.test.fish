27bc54bc83bc0e99fc4af9bae58a12fece340b29
Edouard Lopez <contact@edouard-lopez.com>
2018-09-08 12:44:19 +0200

    simplify code

--- tests/ci.test.fish
+++ tests/ci.test.fish
@@ -2,22 +2,13 @@ function setup
     docker pull edouardlopez/pure-fish
 end
 
-begin
-    set -l IFS
-    set fisher_install (
+test "install with fisher"
+    'Done' = (
         docker run \
             --name pure \
             --rm \
             --tty \
-        edouardlopez/pure-fish 'fisher rafaelrinaldi/pure'
+        edouardlopez/pure-fish 'fisher rafaelrinaldi/pure' \
+        | grep --only-matching 'Done'
     )
-    echo $fisher_install
 end
-
-test "install with fisher"
-    'Done' = (
-        echo $fisher_install | grep --only-matching 'Done'
-        )
-end
-
-
