1d828852e00460f8f5166f858ea69138bc69643e
Edouard Lopez <contact@edouard-lopez.com>
2018-09-08 12:44:53 +0200

    test manual installer

--- tests/ci.test.fish
+++ tests/ci.test.fish
@@ -1,5 +1,15 @@
 function setup
     docker pull edouardlopez/pure-fish
+
+test "install manually (default behaviour of `docker-pure-fish`)"
+    'version' = (
+        docker run \
+            --name pure \
+            --rm \
+            --tty \
+        edouardlopez/pure-fish 'fish --version' \
+        | grep --only-matching 'version'
+    )
 end
 
 test "install with fisher"
