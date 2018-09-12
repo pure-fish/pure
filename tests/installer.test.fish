bae3b465a8f1ce553de0475d89de530cf7f661a0
Edouard Lopez <contact@edouard-lopez.com>
2018-09-12 17:25:49 +0200

    test installer method set_pure_install_path

--- tests/installer.test.fish
+++ tests/installer.test.fish
@@ -13,3 +13,17 @@ test "pass argument to set $FISH_CONFIG_DIR"
         echo "$FISH_CONFIG_DIR"
     )
 end
+
+test 'set $PURE_INSTALL_DIR to default value'
+    "$FISH_CONFIG_DIR/functions/theme-pure" = (
+        pure::set_pure_install_path
+        echo $PURE_INSTALL_DIR
+    )
+end
+
+test "pass arguments to set $PURE_INSTALL_DIR"
+    "/custom/theme/path" = (
+        pure::set_pure_install_path "/custom/config/path" "/custom/theme/path"
+        echo "$PURE_INSTALL_DIR"
+    )
+end
