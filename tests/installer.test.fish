ed28ec24320235ede600e7ffce66087915c9b328
Edouard Lopez <contact@edouard-lopez.com>
2018-09-13 00:40:54 +0200

    reset default config after custom tests

--- tests/installer.test.fish
+++ tests/installer.test.fish
@@ -1,12 +1,5 @@
 source $DIRNAME/../installer.fish
 
-test 'set $FISH_CONFIG_DIR to default value'
-    "$HOME/.config/fish" = (
-        pure::set_fish_config_path
-        echo "$FISH_CONFIG_DIR"
-    )
-end
-
 test "pass argument to set $FISH_CONFIG_DIR"
     "/custom/config/path" = (
         pure::set_fish_config_path "/custom/config/path"
@@ -14,10 +7,10 @@ test "pass argument to set $FISH_CONFIG_DIR"
     )
 end
 
-test 'set $PURE_INSTALL_DIR to default value'
-    "$FISH_CONFIG_DIR/functions/theme-pure" = (
-        pure::set_pure_install_path
-        echo $PURE_INSTALL_DIR
+test 'set $FISH_CONFIG_DIR to default value'
+    "$HOME/.config/fish" = (
+        pure::set_fish_config_path
+        echo "$FISH_CONFIG_DIR"
     )
 end
 
@@ -25,6 +18,13 @@ test "pass arguments to set $PURE_INSTALL_DIR"
     "/custom/theme/path" = (
         pure::set_pure_install_path "/custom/config/path" "/custom/theme/path"
         echo "$PURE_INSTALL_DIR"
+    )
+end
+
+test 'set $PURE_INSTALL_DIR to default value'
+    "$FISH_CONFIG_DIR/functions/theme-pure" = (
+        pure::set_pure_install_path
+        echo $PURE_INSTALL_DIR
     )
 end
 
