2998cf802f50d7ada2bc968a1ec367616b044c70
Edouard Lopez <contact@edouard-lopez.com>
2018-12-10 01:20:58 +0100

    use long format option

--- functions/__format_time.fish
+++ functions/__format_time.fish
@@ -1,11 +1,11 @@
 function __format_time -d "Format milliseconds to a human readable format"
-    set -l milliseconds $argv[1]
-    set -l seconds (math -s0 "$milliseconds / 1000 % 60")
-    set -l minutes (math -s0 "$milliseconds / 60000 % 60")
-    set -l hours (math -s0 "$milliseconds / 3600000 % 24")
-    set -l days (math -s0 "$milliseconds / 86400000")
-    set -l time
-    set -l threshold $argv[2]
+    set --local milliseconds $argv[1]
+    set --local seconds (math -s0 "$milliseconds / 1000 % 60")
+    set --local minutes (math -s0 "$milliseconds / 60000 % 60")
+    set --local hours (math -s0 "$milliseconds / 3600000 % 24")
+    set --local days (math -s0 "$milliseconds / 86400000")
+    set --local time
+    set --local threshold $argv[2]
 
     if test $days -gt 0
         set time (printf "$time%sd " $days)
