7ff2042e105347e2d773ee32a96947c40f2fd186
Nick Murphy <comfortablynick@gmail.com>
2018-12-12 15:35:58 -0600

    Add check for existing fish `hostname` variable

--- functions/_pure_prompt_host.fish
+++ functions/_pure_prompt_host.fish
@@ -1,5 +1,6 @@
 function _pure_prompt_host
-    set --local hostname (hostname -s) # current host name
+    set -qg hostname
+    or set --local hostname (hostname -s) # current host name
     set --local hostname_color "$pure_host_color"
 
     echo "$hostname_color$hostname"
