851b37c0439d8f3354c3894478f44793211bd95d
Edouard Lopez <contact@edouard-lopez.com>
2018-12-12 22:22:16 +0100

    test _pure_command_duration_prompt
    see #96

---
+++ tests/_pure_prompt_command_duration.test.fish
@@ -0,0 +1,23 @@
+source $DIRNAME/../functions/_pure_format_time.fish
+source $DIRNAME/../functions/_pure_prompt_command_duration.fish
+
+set --local EMPTY ''
+
+test "hide command duration when it's zero"
+    (
+        set --local CMD_DURATION $EMPTY
+        set pure_color_yellow $EMPTY
+
+        _pure_prompt_command_duration
+    ) = $EMPTY
+end
+
+test "displays command duration when non-zero"
+    (
+        set CMD_DURATION 6000 # in milliseconds
+        set pure_command_max_exec_time 5 # in seconds
+        set pure_color_yellow $EMPTY
+
+        _pure_prompt_command_duration
+    ) = '6s'
+end
