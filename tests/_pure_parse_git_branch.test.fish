cd9912047b1d622d2af9c254d0c53d55b148382e
Edouard Lopez <contact@edouard-lopez.com>
2018-12-13 00:14:58 +0100

    test _pure_parse_git_branch.fish
    see #96

---
+++ tests/_pure_parse_git_branch.test.fish
@@ -0,0 +1,25 @@
+source $DIRNAME/../functions/_pure_parse_git_branch.fish
+
+set temporary_test_directory /tmp/pure
+
+function setup
+    rm --recursive --force $temporary_test_directory
+    mkdir -p $temporary_test_directory
+    cd $temporary_test_directory
+    git init --quiet
+    touch readme.md
+    git add .
+    git commit --message='init mock git' --quiet
+end
+
+test "returns current branch"
+    (
+        cd $temporary_test_directory
+
+        _pure_parse_git_branch
+    ) = 'master'
+end
+
+function teardown
+    rm --recursive --force $temporary_test_directory
+end
