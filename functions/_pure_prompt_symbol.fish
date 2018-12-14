a6264be803c42b1d47dd2626a2d10c8e06e67bef
schrodincat <schrodinger_cat@me.com>
2018-12-14 19:03:27 +0300

    Change prompt symbol default color to magenta

--- functions/_pure_prompt_symbol.fish
+++ functions/_pure_prompt_symbol.fish
@@ -5,11 +5,11 @@ function _pure_prompt_symbol \
     set --local pure_symbol $pure_symbol_prompt
     set --local command_succeed 0
 
-    set --local color_symbol $pure_color_green  # pure symbol is green by default
+    set --local color_symbol $pure_color_magenta # pure symbol is magenta by default
     if test $exit_code -ne 0
         set color_symbol $pure_color_red  # pure symbol is red when previous command failed
 
-        if test $pure_separate_prompt_on_error = true 
+        if test $pure_separate_prompt_on_error = true
             set color_symbol "$pure_color_red$pure_symbol_prompt$pure_color_magenta"
         end
     end
