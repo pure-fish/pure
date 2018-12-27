ed6c25546a36422c62e117b0050f0b7bbef02949
Edouard Lopez <contact@edouard-lopez.com>
2018-12-28 00:17:52 +0100

    use fish 3 syntax for redirection in _pure_prompt_git.fish

--- functions/_pure_prompt_git.fish
+++ functions/_pure_prompt_git.fish
@@ -1,6 +1,6 @@
 function _pure_prompt_git
     # Check if is on a Git repository
-    set --local is_git_repository (command git rev-parse --is-inside-work-tree ^/dev/null)
+    set --local is_git_repository (command git rev-parse --is-inside-work-tree 2>/dev/null)
     if test -n "$is_git_repository"
         set --local git_prompt (_pure_prompt_git_branch)(_pure_prompt_git_dirty)
         set --local git_arrows (_pure_prompt_git_arrows)
