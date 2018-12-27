58d6bb6144b278a629012d5a25157e36a08b6111
Edouard Lopez <contact@edouard-lopez.com>
2018-12-27 23:55:47 +0100

    fix _pure_prompt_git_arrows.test.fish in CI, by setting $pure_color_cyan

--- tests/_pure_prompt_git_arrows.test.fish
+++ tests/_pure_prompt_git_arrows.test.fish
@@ -7,11 +7,17 @@ set -U fish_features stderr-nocaret
 
 function setup
     rm --recursive --force $fake_git_repo
+
+    git init --bare --quiet /tmp/pure.git
     mkdir -p $fake_git_repo
     cd $fake_git_repo
     git init --quiet
     git config --local user.email "you@example.com"
     git config --local user.name "Your Name"
+    git remote add origin ../pure.git/ 
+    touch file.txt
+    git add file.txt
+    git commit --quiet --message='init'
 end
 
 function teardown
@@ -30,31 +36,21 @@ end
 
 test "show arrow UP when branch is AHEAD of upstream (need git push)"
     (
-        git init --bare --quiet /tmp/pure.git
-        cd $fake_git_repo
-        git remote add origin ../pure.git/ 
-        touch file.txt
-        git add file.txt
-        git commit --quiet --message='init'
         git push --set-upstream --quiet origin master > /dev/null
-
         touch missing-on-upstream.txt
         git add missing-on-upstream.txt
         git commit --quiet --message='missing on upstream'
 
         set pure_symbol_git_up_arrow '^'
+        set pure_color_cyan (set_color cyan)
+
         _pure_prompt_git_arrows
+
     ) = (set_color cyan)'^'
 end
 
 test "show arrow DOWN when branch is BEHIND upstream (need git pull)"
     (
-        git init --bare --quiet /tmp/pure.git
-        cd $fake_git_repo
-        git remote add origin ../pure.git/ 
-        touch file.txt
-        git add file.txt
-        git commit --quiet --message='init'
         touch another-file.txt
         git add another-file.txt
         git commit --quiet --message='another'
@@ -63,6 +59,8 @@ test "show arrow DOWN when branch is BEHIND upstream (need git pull)"
         git reset --hard --quiet HEAD~1
 
         set pure_symbol_git_down_arrow 'v'
+        set pure_color_cyan (set_color cyan)
+
         _pure_prompt_git_arrows
     ) = (set_color cyan)'v'
 end
