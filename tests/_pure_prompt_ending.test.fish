bc758f3cc3f19c0dac142fa996c91e1d2b4d4378
Edouard Lopez <contact@edouard-lopez.com>
2018-12-27 23:49:54 +0100

    fix _pure_prompt_ending.test.fish in CI by setting $pure_color_normal

--- tests/_pure_prompt_ending.test.fish
+++ tests/_pure_prompt_ending.test.fish
@@ -2,6 +2,8 @@ source $DIRNAME/../functions/_pure_prompt_ending.fish
 
 test "reset color to normal"
     (
+        set pure_color_normal (set_color normal)
+        
         _pure_prompt_ending
     ) = (set_color normal)' '
 end
