d60c6a8748a181c9cdb1a1d5e6a8768e8808a02b
Edouard Lopez <contact@edouard-lopez.com>
2018-12-28 00:09:32 +0100

    use long format --description

--- functions/_pure_parse_git_branch.fish
+++ functions/_pure_parse_git_branch.fish
@@ -1,4 +1,4 @@
-function _pure_parse_git_branch -d "Parse current Git branch name"
+function _pure_parse_git_branch --description "Parse current Git branch name"
     command git symbolic-ref --short HEAD 2>/dev/null;
         or echo (command git show-ref --head -s --abbrev HEAD)[1]
 end
