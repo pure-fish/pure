e32be7cdd68a24aca084a3774d60ac387dfa2dd6
Edouard Lopez <contact@edouard-lopez.com>
2018-12-28 00:24:47 +0100

    test _pure_prompt_git_dirty.fish

---
+++ tests/_pure_prompt_git_dirty.test.fish
@@ -0,0 +1,36 @@
+source $DIRNAME/../functions/_pure_prompt_git_dirty.fish
+source $DIRNAME/../functions/_pure_parse_git_branch.fish
+
+
+function setup
+    rm --recursive --force /tmp/pure
+
+    mkdir -p /tmp/pure
+    cd /tmp/pure
+
+    git init --quiet
+    git config --local user.email "you@example.com"
+    git config --local user.name "Your Name"
+end
+
+test "untracked files make git repo as dirty"
+    (
+        touch file.txt
+        set pure_symbol_git_dirty '*'
+        
+        _pure_prompt_git_dirty
+    ) = '*'
+end
+
+test "staged files mark git repo as dirty"
+    (
+        touch file.txt
+        git add file.txt
+        set pure_symbol_git_dirty '*'
+        
+        _pure_prompt_git_dirty
+    ) = '*'
+end
+function teardown
+    rm --recursive --force /tmp/pure
+end
