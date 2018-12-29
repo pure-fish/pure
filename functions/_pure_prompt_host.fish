cdd35a42f08861880dd079c8bedef02550f88e05
Edouard Lopez <contact@edouard-lopez.com>
2018-12-29 23:59:16 +0100

    refactor _pure_prompt_host.fish

--- functions/_pure_prompt_host.fish
+++ functions/_pure_prompt_host.fish
@@ -1,6 +1,6 @@
 function _pure_prompt_host
-    set -qg hostname
-    or set --local hostname (hostname -s) # current host name
+    set --query --global hostname
+    or set --local hostname (hostname --short) # current host name
     set --local hostname_color "$pure_host_color"
 
     echo "$hostname_color$hostname"
