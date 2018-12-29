eaa921f41cbfd90db4e42396beb4a0463fccd32b
Edouard Lopez <contact@edouard-lopez.com>
2018-12-29 21:01:09 +0100

    test _pure_prompt_user.fish

--- functions/_pure_prompt_user.fish
+++ functions/_pure_prompt_user.fish
@@ -1,6 +1,7 @@
 function _pure_prompt_user
-    set --local username (whoami) # current user name
+    set --local username (whoami)
     set --local username_color "$pure_username_color"  # default color
+    
     if [ "$username" = "root" ]
         set username_color "$pure_root_color" # different color for root
     end
