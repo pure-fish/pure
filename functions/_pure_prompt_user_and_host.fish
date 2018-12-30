0e1bf2484757cf4c7409cc86edbe542198a67c93
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 10:33:44 +0100

    refactor _pure_prompt_user_and_host.fish

--- functions/_pure_prompt_user_and_host.fish
+++ functions/_pure_prompt_user_and_host.fish
@@ -1,5 +1,5 @@
 function _pure_prompt_user_and_host
-    if [ "$SSH_CONNECTION" != "" ]
+    if test -n "$SSH_CONNECTION"  # we are on a host through SSH
         echo (_pure_prompt_user)(_pure_prompt_at)(_pure_prompt_host)
     end
 end
