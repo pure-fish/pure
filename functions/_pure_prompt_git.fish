daf07edc0d28072ee31facf121b98d5eeb6a19fb
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 22:49:58 +0100

    add description to _pure_prompt_git.fish

--- functions/_pure_prompt_git.fish
+++ functions/_pure_prompt_git.fish
@@ -1,12 +1,16 @@
-function _pure_prompt_git
-    # Check if is on a Git repository
+function _pure_prompt_git \
+    --description 'Print git repository informations: branch name, dirty, upstream ahead/behind'
+
     set --local is_git_repository (command git rev-parse --is-inside-work-tree 2>/dev/null)
+
     if test -n "$is_git_repository"
         set --local git_prompt (_pure_prompt_git_branch)(_pure_prompt_git_dirty)
         set --local git_arrows (_pure_prompt_git_arrows)
+
         if test (_pure_string_width $git_arrows) -ne 0
             set git_prompt $git_prompt $git_arrows
         end
+
         echo $git_prompt
     end
 end
