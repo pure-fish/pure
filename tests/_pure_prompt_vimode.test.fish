89162e87a0292a7d6efc678ca529d115548489b1
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 11:09:25 +0100

    test default vi_mode for _pure_prompt_vimode.fish

--- tests/_pure_prompt_vimode.test.fish
+++ tests/_pure_prompt_vimode.test.fish
@@ -2,8 +2,16 @@ source $DIRNAME/../functions/_pure_prompt_vimode.fish
 
 set --local EMPTY ''
 
-test "hide vimode by default"
+test "hides vimode prompt by default"
     (
         _pure_prompt_vimode
     ) = $EMPTY
+end
+
+test "shows default vimode prompt"
+    (
+        set fish_key_bindings fish_vi_key_bindings
+
+        _pure_prompt_vimode
+    ) = (set_color --bold --background red white)'[N] '(set_color normal)' '
 end
