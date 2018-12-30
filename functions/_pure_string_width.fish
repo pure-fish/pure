b02d0ed78d770ac9971191b25830c3a0fcca0443
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 11:40:48 +0100

    refactor _pure_string_width.fish

--- functions/_pure_string_width.fish
+++ functions/_pure_string_width.fish
@@ -1,3 +1,9 @@
-function _pure_string_width
-    echo (string length (string replace -ra '\e\[[^m]*m' '' $argv[1]))
+function _pure_string_width \
+    --description 'returns raw string length, i.e. ignore ANSI-color' \
+    --argument-names prompt
+
+    set --local empty ''
+    set --local raw_prompt (string replace --all --regex '\e\[[^m]*m' $empty $prompt)
+
+    string length $raw_prompt
 end
