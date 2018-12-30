a4bea8dee6df364ac705b06b244291554448fa54
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 23:25:42 +0100

    add --argument-names and --description to _pure_prompt.fish

--- functions/_pure_prompt.fish
+++ functions/_pure_prompt.fish
@@ -1,7 +1,10 @@
-function _pure_prompt
+function _pure_prompt \
+    --description 'Print prompt symbol' \
+    --argument-names exit_code
+
     set --local virtualenv (_pure_prompt_virtualenv) # Python virtualenv name
     set --local vimode_indicator (_pure_prompt_vimode) # vi-mode indicator
-    set --local pure_symbol (_pure_prompt_symbol $argv[1])
+    set --local pure_symbol (_pure_prompt_symbol $exit_code)
 
     echo (_pure_print_prompt $virtualenv $vimode_indicator $pure_symbol)
 end
