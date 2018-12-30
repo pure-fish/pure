6f243eb77b575e8366ee2ce28381351ec6967de0
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 12:48:20 +0100

    add description to _pure_prompt_virtualenv.fish

--- functions/_pure_prompt_virtualenv.fish
+++ functions/_pure_prompt_virtualenv.fish
@@ -1,4 +1,4 @@
-function _pure_prompt_virtualenv
+function _pure_prompt_virtualenv --description "Display virtualenv directory"
     if test -n "$VIRTUAL_ENV"
         set --local virtualenv (basename "$VIRTUAL_ENV")
         set --local virtualenv_color "$pure_color_gray"
