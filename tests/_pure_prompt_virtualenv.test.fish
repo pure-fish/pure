a99546534ea0f84281a3ce8b059f5b0835ed4b43
Edouard Lopez <contact@edouard-lopez.com>
2018-12-30 12:47:25 +0100

    test _pure_prompt_virtualenv.fish

---
+++ tests/_pure_prompt_virtualenv.test.fish
@@ -0,0 +1,20 @@
+source $DIRNAME/../functions/_pure_prompt_virtualenv.fish
+
+test "hide virtualenv prompt when not activated"
+    (
+        set --erase VIRTUAL_ENV
+
+        _pure_prompt_virtualenv
+
+    ) $status -eq 0
+end
+
+test "displays virtualenv directory prompt"
+    (
+        set VIRTUAL_ENV /home/test/fake/project/
+        set pure_color_gray (set_color grey)
+
+        _pure_prompt_virtualenv
+
+    ) = (set_color grey)'project'
+end
