9752f603f0049a066b93972fe1862f51a4760679
schrodincat <schrodinger_cat@me.com>
2019-01-03 04:05:02 +0300

    Remove unnecessary files

--- functions/_pure_prompt_user.fish
+++
@@ -1,9 +0,0 @@
-function _pure_prompt_user
-    set --local username (whoami)
-    set --local username_color "$pure_color_ssh_username"  # default color
-    if [ "$username" = "root" ]
-        set username_color "$pure_color_ssh_root" # different color for root
-    end
-
-    echo "$username_color$username"
-end
--- functions/_pure_prompt_user_and_host.fish
+++
@@ -1,5 +0,0 @@
-function _pure_prompt_user_and_host
-    if test -n "$SSH_CONNECTION"  # we are on a host through SSH
-        echo (_pure_prompt_user)(_pure_prompt_at)(_pure_prompt_host)
-    end
-end
