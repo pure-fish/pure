5d2f9c6fa99827bbc8149fdba91b833b15d69aaf
Edouard Lopez <contact@edouard-lopez.com>
2019-01-05 00:50:20 +0100

    clean code

--- tools/installer.fish
+++ tools/installer.fish
@@ -49,10 +49,9 @@ end
 
 function pure::enable_autoloading
     printf "\tEnabling autoloading for pure's functions on shell init"
-    set -l marker "# THEME PURE #"
     touch "$FISH_CONFIG_DIR/config.fish"
     if not test (grep "$THEME_PURE" $FISH_CONFIG_DIR/config.fish 2>&1 >/dev/null)
-        echo "$marker" >> $FISH_CONFIG_DIR/config.fish
+        echo "# THEME PURE #" >> $FISH_CONFIG_DIR/config.fish
         echo "set fish_function_path $PURE_INSTALL_DIR/functions/" '$fish_function_path' >> $FISH_CONFIG_DIR/config.fish
     end
     ln -sf $PURE_INSTALL_DIR/fish_prompt.fish $FISH_CONFIG_DIR/functions/
