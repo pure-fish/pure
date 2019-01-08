97c9c4de7bc4261f5992d77106b60cb88e75ddb4
Edouard Lopez <contact@edouard-lopez.com>
2019-01-08 13:41:23 +0100

    refactor fish_title.fish

--- fish_title.fish
+++ fish_title.fish
@@ -1,12 +1,15 @@
-# Set title to current folder and shell name
-function fish_title
+function fish_title \
+    --description "Set title to current folder and shell name" \
+    --argument-names last_command
+
     set --local basename (string replace -r '^.*/' '' -- $PWD)
     set --local current_folder (_pure_parse_directory)
-    set --local command $argv[1]
-    set --local prompt "$basename: $command $pure_symbol_horizontal_bar $_"
+    set --local current_command (status current-command 2>/dev/null; or echo $_)
 
-    if test -z "$command"
-        set prompt "$current_folder $pure_symbol_horizontal_bar $_"
+    set --local prompt "$basename: $last_command $pure_symbol_horizontal_bar $current_command"
+
+    if test -z "$last_command"
+        set prompt "$current_folder $pure_symbol_horizontal_bar $current_command"
     end
 
     echo $prompt
