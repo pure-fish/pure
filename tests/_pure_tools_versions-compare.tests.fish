9d4aec015135817dbb587f7a446e6c6ab1c4cb65
Edouard Lopez <contact@edouard-lopez.com>
2019-01-10 23:23:24 +0100

    refactor fish_version_at_least and fish_version_below
    Signed-off-by: Edouard Lopez <contact@edouard-lopez.com>

--- tests/_pure_tools_versions-compare.tests.fish
+++ tests/_pure_tools_versions-compare.tests.fish
@@ -3,50 +3,44 @@ source $DIRNAME/../tools/versions-compare.fish
 set --local fail 1
 set --local success 0
 
-test "versions: is_lower_than_equal fail when first > second argument"
+test "fish_version_below: fails on itself"
     (
-        is_lower_than_equal '2.5.7' '2.5.6'
+        set mock_fish_version '3.0.0'
+        fish_version_below '3.0.0' $mock_fish_version >/dev/null
     ) $status -eq $fail
 end
 
-test "versions: is_lower_than_equal succeed when first < second argument"
+test "fish_version_below: succeed when FISH_VERSION is lower"
     (
-        is_lower_than_equal '2.5.6' '2.5.7'
+        set mock_fish_version '3.0.0'
+        fish_version_below '4.0.0' $mock_fish_version >/dev/null
     ) $status -eq $success
 end
 
-test "versions: is_lower_than succeed when first = second argument"
+test "fish_version_below: fails when FISH_VERSION is higher"
     (
-        is_lower_than_equal '2.5.7' '2.5.7'
+        set mock_fish_version '4.0.0'
+        fish_version_below '3.0.0' $mock_fish_version >/dev/null
+    ) $status -eq $fail
+end
+
+test "fish_version_at_least: succeed on itself"
+    (
+        set mock_fish_version '3.0.0'
+        fish_version_at_least '3.0.0' $mock_fish_version >/dev/null
     ) $status -eq $success
 end
 
-test "versions: is_lower_than fail when first = second argument"
+test "fish_version_at_least: fails when FISH_VERSION is lower"
     (
-        is_lower_than '2.5.7' '2.5.7'
+        set mock_fish_version '3.0.0'
+        fish_version_at_least '4.0.0' $mock_fish_version >/dev/null
     ) $status -eq $fail
 end
 
-test "versions: is_lower_than succeed when first < second argument"
+test "fish_version_at_least: succeed when FISH_VERSION is higher"
     (
-        is_lower_than '2.5.6' '2.5.7'
-    ) $status -eq $success
-end
-
-test "versions: is_lower_than fail when first > second argument"
-    (
-        is_lower_than '2.5.7' '2.5.6'
-    ) $status -eq $fail
-end
-
-test "versions: fish_version_below current version fail on itself"
-    (
-        fish_version_below $FISH_VERSION >/dev/null
-    ) $status -eq $fail
-end
-
-test "versions: fish_version_at_least current version succeed on itself"
-    (
-        fish_version_at_least $FISH_VERSION >/dev/null
+        set mock_fish_version '4.0.0'
+        fish_version_at_least '3.0.0' $mock_fish_version >/dev/null
     ) $status -eq $success
 end
--- tools/versions-compare.fish
+++ tools/versions-compare.fish
@@ -1,39 +1,30 @@
 set --global fail 1
 set --global success 0
 
-function is_lower_than_equal
-    set lowest (printf "$argv[1]\n$argv[2]" | sort --version-sort | head -n1)
-    test $argv[1] = $lowest
-end
-
-function is_lower_than
-    if test $argv[1] = $argv[2]
-        return $fail
-    else
-        is_lower_than_equal $argv[1] $argv[2]
-    end
-end
-
 function fish_version_below \
-    --description 'True if $FISH_VERSION is lower than submitted version. Targeted to be used in test, hence the `printf`.' \
-    --argument-names version
-    
-    if test (is_lower_than $FISH_VERSION $version)
-        printf "\n%sfish <%s: %s" (set_color blue) $version (set_color normal)
-        return $success
-    else
+    --description "Compare versions.  By default this tests \$FISH_VERSION, but if a second argument is provided it tests against that version." \
+    --argument-names expected actual
+
+	if test -z "$actual"
+		set actual $FISH_VERSION
+	end
+
+    if test $expected = $actual
         return $fail
     end
+
+	printf '%s\n' $actual $expected | sort --check=silent --version-sort
+    and printf "\n%sfish <%s: %s" (set_color blue) $expected (set_color normal)
+
+	return $status
 end
 
 function fish_version_at_least \
-    --description 'True if $FISH_VERSION is greater or equal than submitted version. Targeted to be used in test, hence the `printf`.' \
-    --argument-names version
-    
-    if test (fish_version_below $version >/dev/null)
-        return $fail
-    else 
-        printf "\n%sfish ≥%s: %s" (set_color blue) $version (set_color normal)
-        return $success
-    end
+    --description "Compare versions.  By default this tests \$FISH_VERSION, but if a second argument is provided it tests against that version." \
+    --argument-names expected actual
+
+    not fish_version_below $argv
+    and printf "\n%sfish ≥%s: %s" (set_color blue) $expected (set_color normal)
+
+    return $status
 end
