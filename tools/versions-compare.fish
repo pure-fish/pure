ea29192ad552ed8fcb60143f930c37976046a11c
Edouard Lopez <contact@edouard-lopez.com>
2019-01-10 20:54:20 +0100

    explicit return value for `fish_version_at_least` and `fish_version_below` and print version

--- tools/versions-compare.fish
+++ tools/versions-compare.fish
@@ -1,3 +1,6 @@
+set --global fail 1
+set --global success 0
+
 function is_lower_than_equal
     set lowest (printf "$argv[1]\n$argv[2]" | sort --version-sort | head -n1)
     test $argv[1] = $lowest
@@ -5,20 +8,34 @@ end
 
 function is_lower_than
     if test $argv[1] = $argv[2]
-     return 1
+        return $fail
     else
         is_lower_than_equal $argv[1] $argv[2]
     end
 end
 
-function fish_version_below --argument-names version
-    is_lower_than $FISH_VERSION $version
+function fish_version_below \
+    --description 'True if $FISH_VERSION is lower than submitted version. Targeted to be used in test, hence the `printf`.' \
+    --argument-names version
+    
+    if test (is_lower_than $FISH_VERSION $version)
+        printf  "[fish <%s]" $version
+        return $success
+    else
+        printf  "[fish ≥%s]" $version
+        return $fail
+    end
 end
 
-function fish_version_at_least --argument-names version
-    if test (fish_version_below $version)
-        return 1
+function fish_version_at_least \
+    --description 'True if $FISH_VERSION is greater or equal than submitted version. Targeted to be used in test, hence the `printf`.' \
+    --argument-names version
+    
+    if test (fish_version_below $version >/dev/null)
+        printf  "[fish ≥%s]" $version
+        return $fail
     else 
-        return 0
+        printf  "[fish <%s]" $version
+        return $success
     end
 end
