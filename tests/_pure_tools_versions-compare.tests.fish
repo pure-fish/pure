2969cb9d5fc14b8e8befa6e853e9bd732a585d30
Edouard Lopez <contact@edouard-lopez.com>
2019-01-10 20:56:11 +0100

    explicit tested value

--- tests/_pure_tools_versions-compare.tests.fish
+++ tests/_pure_tools_versions-compare.tests.fish
@@ -1,49 +1,52 @@
 source $DIRNAME/../tools/versions-compare.fish
 
+set --local fail 1
+set --local success 0
+
 test "versions: is_lower_than_equal fail when first > second argument"
     (
         is_lower_than_equal '2.5.7' '2.5.6'
-    ) $status -eq 1
+    ) $status -eq $fail
 end
 
 test "versions: is_lower_than_equal succeed when first < second argument"
     (
         is_lower_than_equal '2.5.6' '2.5.7'
-    ) $status -eq 0
+    ) $status -eq $success
 end
 
 test "versions: is_lower_than succeed when first = second argument"
     (
         is_lower_than_equal '2.5.7' '2.5.7'
-    ) $status -eq 0
+    ) $status -eq $success
 end
 
 test "versions: is_lower_than fail when first = second argument"
     (
         is_lower_than '2.5.7' '2.5.7'
-    ) $status -eq 1
+    ) $status -eq $fail
 end
 
 test "versions: is_lower_than succeed when first < second argument"
     (
         is_lower_than '2.5.6' '2.5.7'
-    ) $status -eq 0
+    ) $status -eq $success
 end
 
 test "versions: is_lower_than fail when first > second argument"
     (
         is_lower_than '2.5.7' '2.5.6'
-    ) $status -eq 1
+    ) $status -eq $fail
 end
 
 test "versions: fish_version_below current version fail on itself"
     (
         fish_version_below $FISH_VERSION >/dev/null
-    ) $status -eq 1
+    ) $status -eq $fail
 end
 
 test "versions: fish_version_at_least current version succeed on itself"
     (
         fish_version_at_least $FISH_VERSION >/dev/null
-    ) $status -eq 0
+    ) $status -eq $success
 end
