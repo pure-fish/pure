fe65e014ce65b1830a1319551c72b1e0664e8445
Edouard Lopez <contact@edouard-lopez.com>
2019-01-10 21:18:46 +0100

    condition _pure_prompt_ssh.fish test to fish version
    fixes #124

--- tests/_pure_prompt_ssh.test.fish
+++ tests/_pure_prompt_ssh.test.fish
@@ -2,6 +2,7 @@ source $DIRNAME/../functions/_pure_prompt_ssh.fish
 source $DIRNAME/../functions/_pure_prompt_ssh_user.fish
 source $DIRNAME/../functions/_pure_prompt_ssh_separator.fish
 source $DIRNAME/../functions/_pure_prompt_ssh_host.fish
+source $DIRNAME/../tools/versions-compare.fish
 
 set --local empty ''
 
@@ -14,18 +15,34 @@ test "hide 'user@hostname' when working locally"
     ) $status -eq 0
 end
 
+if fish_version_below '3.0.0'
 test "displays 'user@hostname' when on SSH connection"
     (
         set pure_color_ssh_user_normal $empty
         set pure_color_ssh_separator $empty
         set pure_color_ssh_host $empty
         set SSH_CONNECTION 127.0.0.1 56422 127.0.0.1 22
-        function whoami  # mock
-            echo 'user'
-        end
+        function whoami; echo 'user'; end # mock
         set hostname 'hostname'
 
         _pure_prompt_ssh
 
     ) = 'user@hostname'
+end
+end
+
+if fish_version_at_least '3.0.0'
+test "displays 'user@[\w]+' when on SSH connection"
+    (
+        set pure_color_ssh_user_normal $empty
+        set pure_color_ssh_separator $empty
+        set pure_color_ssh_host $empty
+        set SSH_CONNECTION 127.0.0.1 56422 127.0.0.1 22
+        function whoami; echo 'user'; end # mock
+
+        set prompt_ssh_host (_pure_prompt_ssh)
+        string match --quiet --regex 'user@[\w]+' $prompt_ssh_host  
+        # $hostname is read-only, we can't determine it preceisely (e.g. is dynamic in docker container)
+    ) $status -eq 0
+end
 end
