0a391af77515dc27606d85b379f776aadfa1ff6f
Edouard Lopez <contact@edouard-lopez.com>
2019-01-10 17:40:03 +0100

    condition _pure_prompt_ssh_host test to fish version
    fixes #124

--- tests/_pure_prompt_ssh_host.test.fish
+++ tests/_pure_prompt_ssh_host.test.fish
@@ -1,16 +1,19 @@
 source $DIRNAME/../functions/_pure_prompt_ssh_host.fish
+source $DIRNAME/../tools/versions-compare.fish
 
-test "colorize hostname (using reserved variable)"
+if fish_version_below '3.0.0'
+test "_pure_prompt_ssh_host: colorize hostname (using reserved variable) [fish <3.0.0]"
     (
-        set pure_color_ssh_host (set_color grey)
-        set hostname 'hostname-variable'
+            set pure_color_ssh_host (set_color grey)
+            set hostname 'hostname-variable'
 
-        _pure_prompt_ssh_host
-
+            _pure_prompt_ssh_host
     ) = (set_color grey)'hostname-variable'
 end
+end
 
-test "colorize hostname (using hostname executable)"
+if fish_version_below '3.0.0'
+test "_pure_prompt_ssh_host: colorize hostname (using hostname executable) [fish <3.0.0]"
     (
         set --erase hostname
         function hostname  # mock
@@ -23,4 +26,15 @@ test "colorize hostname (using hostname executable)"
 
         functions --erase hostname
     ) = (set_color grey)'hostname-executable'
+end
+end
+
+if fish_version_at_least '3.0.0'
+test "_pure_prompt_ssh_host: use native \$hostname [fish ≥3.0.0]"
+    (
+        set pure_color_ssh_host (set_color grey)
+
+        _pure_prompt_ssh_host > /dev/null
+    ) $status -eq 0
+end
 end
