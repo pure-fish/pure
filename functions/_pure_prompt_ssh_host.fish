1c28f025af5bd2e78ab63a733e3cb0270e33f2ff
Edouard Lopez <contact@edouard-lopez.com>
2019-01-11 22:19:06 +0100

    use short form option to be compatible with busybox

--- functions/_pure_prompt_ssh_host.fish
+++ functions/_pure_prompt_ssh_host.fish
@@ -1,6 +1,6 @@
 function _pure_prompt_ssh_host
     set --query --global hostname
-    or set --local hostname (hostname --short) # current host name
+    or set --local hostname (hostname -s) # current host name compatible busybox
     set --local hostname_color "$pure_color_ssh_host"
 
     echo "$hostname_color$hostname"
