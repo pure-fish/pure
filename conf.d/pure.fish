66d24a754fa9dd6d58f60febf113489b4bf00cff
Edouard Lopez <contact@edouard-lopez.com>
2019-01-15 12:40:28 +0100

    rename `$pure_color_symbol_success` as `$pure_color_prompt_on_success`

--- conf.d/pure.fish
+++ conf.d/pure.fish
@@ -39,7 +39,7 @@ _pure_set_default pure_color_ssh_separator $pure_color_mute
 _pure_set_default pure_color_ssh_user_normal $pure_color_mute
 _pure_set_default pure_color_ssh_user_root $pure_color_light
 _pure_set_default pure_color_prompt_on_error $pure_color_danger
-_pure_set_default pure_color_symbol_success $pure_color_success
+_pure_set_default pure_color_prompt_on_success $pure_color_success
 _pure_set_default pure_color_virtualenv $pure_color_mute
 
 # Print current working directory at the beginning of prompt
--- tests/_pure_prompt_symbol.test.fish
+++ tests/_pure_prompt_symbol.test.fish
@@ -7,7 +7,7 @@ set --local success 0
 test "_pure_prompt_symbol: throws error message when argument is missing"
     (
         set pure_symbol_prompt '>'  # using default ❯ break following tests
-        set pure_color_symbol_success (set_color green)
+        set pure_color_prompt_on_success (set_color green)
 
         set output (_pure_prompt_symbol 2>&1)
         echo $output[1]
@@ -18,7 +18,7 @@ test "_pure_prompt_symbol: colorizes prompt in green when last command succeed"
     (
         set --local last_command $success
         set pure_symbol_prompt '>'  # using default ❯ break following tests
-        set pure_color_symbol_success (set_color green)
+        set pure_color_prompt_on_success (set_color green)
 
         _pure_prompt_symbol $last_command
     ) = (set_color green)'>'
@@ -39,7 +39,7 @@ test "_pure_prompt_symbol: add a magenta prompt when pure_separate_prompt_on_err
         set --local last_command $fail
         set pure_symbol_prompt '>'  # using default ❯ break following tests
         set pure_color_prompt_on_error (set_color red)
-        set pure_color_symbol_success (set_color magenta)
+        set pure_color_prompt_on_success (set_color magenta)
         set pure_separate_prompt_on_error true
 
 
