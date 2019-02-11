58e63dc0421d4fd824eaa51a1288df0bef838f25
Daniel Sockwell <dsockwell@gmail.com>
2019-02-11 10:24:46 -0500

    Simplify code testing settings for vi_mode symbol

--- functions/_pure_prompt_symbol.fish
+++ functions/_pure_prompt_symbol.fish
@@ -2,33 +2,23 @@ function _pure_prompt_symbol \
     --description 'Print prompt symbol' \
     --argument-names exit_code
 
-    set --local reverse_symbol_instead_of_default_mode_prompt $pure_reverse_prompt_symbol_in_vimode
-    set --local pure_symbol $pure_symbol_prompt
-    set --local pure_symbol_reverse $pure_symbol_reverse
-
+    set --local pure_symbol $pure_symbol_prompt # default pure symbol (`❯`)
+    set --local is_vi_mode (string match fish_{vi,hybrid}_key_bindings $fish_key_bindings)
+    if test is_vi_mode \
+      -a $pure_reverse_prompt_symbol_in_vimode \
+      -a "$fish_bind_mode" != "insert"
+        set pure_symbol $pure_symbol_reverse # default reverse symbol `❮`
+    end
     set --local command_succeed 0
 
     set --local color_symbol $pure_color_prompt_on_success # default pure symbol color
     if test $exit_code -ne 0
         set color_symbol $pure_color_prompt_on_error  # different pure symbol color when previous command failed
 
-        if test "$pure_separate_prompt_on_error" = true
-            set color_symbol "$pure_color_prompt_on_error$pure_symbol_prompt$pure_color_prompt_on_success"
+        if test $pure_separate_prompt_on_error = true
+            set color_symbol "$pure_color_symbol_error$pure_symbol$pure_color_symbol_success"
         end
     end
 
-    if test reverse_symbol_instead_of_default_mode_prompt
-      # Do nothing if not in vi mode
-      if test "$fish_key_bindings" = "fish_vi_key_bindings"
-          or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
-          if test "$fish_bind_mode" = "insert"
-              echo "$color_symbol$pure_symbol"
-          else
-              echo "$color_symbol$pure_symbol_reverse"
-          end
-      end
-
-    else
-      echo "$color_symbol$pure_symbol"
-    end
+    echo "$color_symbol$pure_symbol"
 end
