78482e8520578ac33d415ac661089c64a900dd27
Daniel Sockwell <dsockwell@gmail.com>
2019-02-11 10:24:46 -0500

    Simplify code testing settings for vi_mode symbol

--- functions/_pure_prompt_vimode.fish
+++ functions/_pure_prompt_vimode.fish
@@ -1,6 +1,5 @@
 function _pure_prompt_vimode
-    set --local reverse_symbol_instead_of_default_mode_prompt $pure_reverse_prompt_symbol_in_vimode
-    if test ! reverse_symbol_instead_of_default_mode_prompt
+    if test ! $pure_reverse_prompt_symbol_in_vimode
       echo (fish_default_mode_prompt)
     end
 end
