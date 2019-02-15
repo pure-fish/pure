ad90f11f0928a5190dd03bbb4f0c7ab6887d785d
Your Name <you@example.com>
2019-02-15 21:26:51 +0100

    quote variable in test

--- functions/_pure_prompt_symbol.fish
+++ functions/_pure_prompt_symbol.fish
@@ -9,7 +9,7 @@ function _pure_prompt_symbol \
     if test $exit_code -ne 0
         set color_symbol $pure_color_prompt_on_error  # different pure symbol color when previous command failed
 
-        if test $pure_separate_prompt_on_error = true
+        if test "$pure_separate_prompt_on_error" = true
             set color_symbol "$pure_color_prompt_on_error$pure_symbol_prompt$pure_color_prompt_on_success"
         end
     end
