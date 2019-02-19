b9714a2e6f7a5b4c49b52c6a60995d85f8e1fcfe
Edouard Lopez <contact@edouard-lopez.com>
2019-02-19 16:50:47 +0100

    quote variable when checking for emptyness

--- functions/_pure_get_prompt_symbol.fish
+++ functions/_pure_get_prompt_symbol.fish
@@ -4,7 +4,7 @@ function _pure_get_prompt_symbol \
 
     set --local prompt_symbol $pure_symbol_prompt
     set --local is_vi_mode (string match fish_{vi,hybrid}_key_bindings $fish_key_bindings)
-    if test is_vi_mode \
+    if test -n "$is_vi_mode" \
             -a "$pure_reverse_prompt_symbol_in_vimode" = true \
             -a "$fish_bind_mode" != "insert"
         set prompt_symbol $pure_symbol_reverse_prompt
