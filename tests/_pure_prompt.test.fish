ef543ef9c79c0e4231a7302d145b31d4256d454d
Edouard Lopez <contact@edouard-lopez.com>
2019-02-19 13:16:18 +0100

    import _pure_get_prompt_symbol.fish in tests

--- tests/_pure_prompt.test.fish
+++ tests/_pure_prompt.test.fish
@@ -2,6 +2,7 @@ source $current_dirname/../functions/_pure_prompt.fish
 source $current_dirname/../functions/_pure_prompt_virtualenv.fish
 source $current_dirname/../functions/_pure_prompt_vimode.fish
 source $current_dirname/../functions/_pure_prompt_symbol.fish
+source $current_dirname/../functions/_pure_get_prompt_symbol.fish
 source $current_dirname/../functions/_pure_print_prompt.fish
 source $current_dirname/../functions/_pure_string_width.fish
 
