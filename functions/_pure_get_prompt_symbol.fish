30cc4be002c015970de7c74ffa1ca019ff8b97a5
Edouard Lopez <contact@edouard-lopez.com>
2019-02-19 15:47:54 +0100

    rename variable to `pure_symbol_reverse_prompt`

--- functions/_pure_get_prompt_symbol.fish
+++ functions/_pure_get_prompt_symbol.fish
@@ -2,12 +2,12 @@ function _pure_get_prompt_symbol \
     --description 'Print prompt symbol' \
     --argument-names exit_code
 
-    set --local prompt_symbol $pure_symbol_prompt # default pure symbol (`❯`)
+    set --local prompt_symbol $pure_symbol_prompt
     set --local is_vi_mode (string match fish_{vi,hybrid}_key_bindings $fish_key_bindings)
     if test is_vi_mode \
             -a "$pure_reverse_prompt_symbol_in_vimode" = true \
             -a "$fish_bind_mode" != "insert"
-        set prompt_symbol $pure_symbol_reverse # default reverse symbol `❮`
+        set prompt_symbol $pure_symbol_reverse_prompt
     end
 
     echo "$prompt_symbol"
--- tests/_pure_get_prompt_symbol.test.fish
+++ tests/_pure_get_prompt_symbol.test.fish
@@ -8,7 +8,7 @@ source $current_dirname/../functions/_pure_get_prompt_symbol.fish
 
 @test "_pure_get_prompt_symbol: get vi-mode symbol ❮" (
     set pure_reverse_prompt_symbol_in_vimode true
-    set pure_symbol_reverse '❮'
+    set pure_symbol_reverse_prompt '❮'
     
     _pure_get_prompt_symbol
 ) = '❮'
