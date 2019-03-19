b31a8a85c34fa08fcbd71dda8cbf7bd74325b607
schrodincat <schrodinger_cat@me.com>
2019-03-19 22:30:07 +0300

    Fix tests for private init variables

--- tests/_pure.test.fish
+++ tests/_pure.test.fish
@@ -8,18 +8,6 @@ set --local empty ''
     echo $pure_version
 ) != $empty
 
-@test "configure: _pure_fresh_session"  (
-    set --erase _pure_fresh_session
-    source $current_dirname/../conf.d/pure.fish
-    echo $_pure_fresh_session
-) = true
-
-@test "configure: VIRTUAL_ENV_DISABLE_PROMPT"  (
-    set --erase VIRTUAL_ENV_DISABLE_PROMPT
-    source $current_dirname/../conf.d/pure.fish
-    echo $VIRTUAL_ENV_DISABLE_PROMPT
-) = 1
-
 @test "configure: pure_symbol_prompt"  (
     set --erase pure_symbol_prompt
     source $current_dirname/../conf.d/pure.fish
@@ -218,3 +206,14 @@ set --local empty ''
     echo $pure_color_right_prompt
 ) = (set_color normal)
 
+@test "configure: VIRTUAL_ENV_DISABLE_PROMPT"  (
+    set --erase VIRTUAL_ENV_DISABLE_PROMPT
+    source $current_dirname/../conf.d/_pure_init.fish
+    echo $VIRTUAL_ENV_DISABLE_PROMPT
+) = 1
+
+@test "configure: _pure_fresh_session"  (
+    set --erase _pure_fresh_session
+    source $current_dirname/../conf.d/_pure_init.fish
+    echo $_pure_fresh_session
+) = true
