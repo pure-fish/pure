9816c5337b0de5e83ca73a874fbece78ff3bcfda
Edouard Lopez <contact@edouard-lopez.com>
2019-03-20 14:13:25 +0100

    remove conf.d/_pure_init.fish tests from tests/_pure.test.fish
    They are already in tests/_pure_init.test.fish

--- tests/_pure.test.fish
+++ tests/_pure.test.fish
@@ -205,15 +205,3 @@ set --local empty ''
     source $current_dirname/../conf.d/pure.fish
     echo $pure_color_right_prompt
 ) = (set_color normal)
-
-@test "configure: VIRTUAL_ENV_DISABLE_PROMPT"  (
-    set --erase VIRTUAL_ENV_DISABLE_PROMPT
-    source $current_dirname/../conf.d/_pure_init.fish
-    echo $VIRTUAL_ENV_DISABLE_PROMPT
-) = 1
-
-@test "configure: _pure_fresh_session"  (
-    set --erase _pure_fresh_session
-    source $current_dirname/../conf.d/_pure_init.fish
-    echo $_pure_fresh_session
-) = true
