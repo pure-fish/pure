4bb29558066d4b9cb6a3c78c12d941259e9740ee
schrodincat <schrodinger_cat@me.com>
2019-03-22 23:12:49 +0300

    Always explicitly specify scope for default values

--- conf.d/pure.fish
+++ conf.d/pure.fish
@@ -12,54 +12,54 @@ _pure_set_default --universal pure_color_warning (set_color yellow)
 _pure_set_default --universal pure_color_dark (set_color black)
 
 # Prompt
-_pure_set_default pure_symbol_prompt "❯"
-_pure_set_default pure_symbol_reverse_prompt "❮"  # used for VI mode
-_pure_set_default pure_color_prompt_on_error $pure_color_danger
-_pure_set_default pure_color_prompt_on_success $pure_color_success
+_pure_set_default --global pure_symbol_prompt "❯"
+_pure_set_default --global pure_symbol_reverse_prompt "❮"  # used for VI mode
+_pure_set_default --global pure_color_prompt_on_error $pure_color_danger
+_pure_set_default --global pure_color_prompt_on_success $pure_color_success
 
 # Current Working Directory
-_pure_set_default pure_color_current_directory $pure_color_primary
+_pure_set_default --global pure_color_current_directory $pure_color_primary
 
 # Git
-_pure_set_default pure_symbol_git_unpulled_commits "⇣"
-_pure_set_default pure_symbol_git_unpushed_commits "⇡"
-_pure_set_default pure_symbol_git_dirty "*"
-_pure_set_default pure_color_git_unpulled_commits $pure_color_info
-_pure_set_default pure_color_git_unpushed_commits $pure_color_info
-_pure_set_default pure_color_git_branch $pure_color_mute
-_pure_set_default pure_color_git_dirty $pure_color_mute
+_pure_set_default --global pure_symbol_git_unpulled_commits "⇣"
+_pure_set_default --global pure_symbol_git_unpushed_commits "⇡"
+_pure_set_default --global pure_symbol_git_dirty "*"
+_pure_set_default --global pure_color_git_unpulled_commits $pure_color_info
+_pure_set_default --global pure_color_git_unpushed_commits $pure_color_info
+_pure_set_default --global pure_color_git_branch $pure_color_mute
+_pure_set_default --global pure_color_git_dirty $pure_color_mute
 
 # SSH info
-_pure_set_default pure_color_ssh_hostname $pure_color_mute
-_pure_set_default pure_color_ssh_separator $pure_color_mute
-_pure_set_default pure_color_ssh_user_normal $pure_color_mute
-_pure_set_default pure_color_ssh_user_root $pure_color_light
+_pure_set_default --global pure_color_ssh_hostname $pure_color_mute
+_pure_set_default --global pure_color_ssh_separator $pure_color_mute
+_pure_set_default --global pure_color_ssh_user_normal $pure_color_mute
+_pure_set_default --global pure_color_ssh_user_root $pure_color_light
 
 # Virtualenv for Pyhon
-_pure_set_default pure_color_virtualenv $pure_color_mute
+_pure_set_default --global pure_color_virtualenv $pure_color_mute
 
 # Print current working directory at the beginning of prompt
 # true (default):   current directory, git, user@hostname (ssh-only), command duration
 # false:            user@hostname (ssh-only), current directory, git, command duration
-_pure_set_default pure_begin_prompt_with_current_directory true
+_pure_set_default --global pure_begin_prompt_with_current_directory true
 
 # Show exit code of last command as a separate prompt character (cf. https://github.com/sindresorhus/pure/wiki#show-exit-code-of-last-command-as-a-separate-prompt-character)
 # false - single prompt character, default
 # true - separate prompt character
-_pure_set_default pure_separate_prompt_on_error false
+_pure_set_default --global pure_separate_prompt_on_error false
 
 # Max execution time of a process before its run time is shown when it exits
-_pure_set_default pure_threshold_command_duration 5
-_pure_set_default pure_color_command_duration $pure_color_warning
+_pure_set_default --global pure_threshold_command_duration 5
+_pure_set_default --global pure_color_command_duration $pure_color_warning
 
 # Right Prompt variables
-_pure_set_default pure_right_prompt ""
-_pure_set_default pure_color_right_prompt $pure_color_normal
+_pure_set_default --global pure_right_prompt ""
+_pure_set_default --global pure_color_right_prompt $pure_color_normal
 
 # VI mode indicator
 # true (default):  indicate a non-insert mode by reversing the prompt symbol (❮)
 # false:           indicate vi mode with [I], [N], [V]
-_pure_set_default pure_reverse_prompt_symbol_in_vimode true
+_pure_set_default --global pure_reverse_prompt_symbol_in_vimode true
 
 # Title
-_pure_set_default pure_symbol_title_bar_separator "—"
+_pure_set_default --global pure_symbol_title_bar_separator "—"
--- functions/_pure_set_default.fish
+++ functions/_pure_set_default.fish
@@ -1,14 +1,4 @@
-function _pure_set_default -S
-    set --local scope "--global"
-    set --local var $argv[1]
-    set --local default $argv[2]
-
-    if test (count $argv) -eq 3
-        set scope $argv[1]
-        set var $argv[2]
-        set default $argv[3]
-    end
-
+function _pure_set_default -S -a scope var default
     if not set -q $var
         set $scope $var $default
     end
