f58dfa13e78205597630f76489725202194c3b13
Harrison Heck <harrison.heck@gmail.com>
2019-05-24 10:27:25 -0400

    test: uncomment fundle tests

--- tests/installation-methods.test.fish
+++ tests/installation-methods.test.fish
@@ -18,7 +18,7 @@ function setup
     if test $USER = 'nemo'
         rm_pure_files
         rm_fish_prompt_files
-        echo '' > $HOME/.config/fish/config.fish
+        echo '' >$HOME/.config/fish/config.fish
     end
 end
 
@@ -29,7 +29,7 @@ if test $USER = 'nemo'
         and install_pure >/dev/null
         source $PURE_INSTALL_DIR/conf.d/*
 
-        fish -c 'fish_prompt | grep -c "❯"' 
+        fish -c 'fish_prompt | grep -c "❯"'
     ) = 1
 end
 
@@ -39,7 +39,7 @@ if test $USER = 'nemo'
         and source /tmp/installer.fish
         and install_pure
 
-        fish -c 'fish_prompt | grep -c "❯"' 
+        fish -c 'fish_prompt | grep -c "❯"'
     ) = 1
 end
 
@@ -47,7 +47,7 @@ if test $USER = 'nemo'
     @test "installation methods: with fisher" (
         fisher add rafaelrinaldi/pure >/dev/null
 
-        fish -c 'fish_prompt | grep -c "❯"' 
+        fish -c 'fish_prompt | grep -c "❯"'
     ) = 1
 end
 
@@ -63,20 +63,20 @@ if test $USER = 'nemo'
                 ln -sf $OMF_PURE_PATH/fish_*.fish $HOME/.config/fish/functions/; \
                 ln -sf $OMF_PURE_PATH/functions/*.fish $HOME/.config/fish/functions/; \
                 ln -sf $OMF_PURE_PATH/conf.d/* $HOME/.config/fish/conf.d/" > /dev/null
-        fish -c "fish_prompt" | grep -c '❯' 
+        fish -c "fish_prompt" | grep -c '❯'
     ) = 1
 end
 
-# if test $USER = 'nemo'
-#     @test "installation methods: with Fundle" (
-#         rm --recursive --force $HOME/.config/fish/fundle/
-#         mkdir -p $HOME/.config/fish/functions
-#         curl https://git.io/fundle --output $HOME/.config/fish/functions/fundle.fish --location --silent >/dev/null
+if test $USER = 'nemo'
+    @test "installation methods: with Fundle" (
+        rm --recursive --force $HOME/.config/fish/fundle/
+        mkdir -p $HOME/.config/fish/functions
+        curl https://git.io/fundle --output $HOME/.config/fish/functions/fundle.fish --location --silent >/dev/null
 
-#         fundle plugin rafaelrinaldi/pure >/dev/null
-#         fundle install >/dev/null
-#         cp $HOME/.config/fish/fundle/rafaelrinaldi/pure/{,functions/}fish_prompt.fish
+        fundle plugin rafaelrinaldi/pure >/dev/null
+        fundle install >/dev/null
+        cp $HOME/.config/fish/fundle/rafaelrinaldi/pure/{,functions/}fish_prompt.fish
 
-#         fish -c 'fish_prompt | grep -c "❯"' 
-#     ) = 1
-# end
+        fish -c 'fish_prompt | grep -c "❯"'
+    ) = 1
+end
