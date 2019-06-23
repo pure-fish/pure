69a461c4c528eaabd21a70b4c22efd4a9e52f321
schrodincat <schrodinger_cat@me.com>
2019-06-23 04:15:13 +0300

    Add iTerm shell integration mark manually

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -3,6 +3,7 @@ function fish_prompt
 
     echo -e -n (_pure_prompt_beginning)  # init prompt context (clear current line, etc.)
     echo -e (_pure_prompt_first_line)  # print current path, git branch/status, command duration
+    _pure_place_iterm2_prompt_mark # place iTerm shell integration mark
     echo -e -n (_pure_prompt $exit_code)  # print prompt
     echo -e (_pure_prompt_ending)  # reset colors and end prompt
 
---
+++ functions/_pure_place_iterm2_prompt_mark.fish
@@ -0,0 +1,5 @@
+function _pure_place_iterm2_prompt_mark
+    if functions -q iterm2_prompt_mark
+        iterm2_prompt_mark
+    end
+end
