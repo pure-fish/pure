b24bd46fc4ea2bf816beb28a2572d0ffaa84d6c3
Tom Forbes <tom@tomforb.es>
2019-06-23 23:24:20 +0100

    Handle virtualenvs that look like flags

--- functions/_pure_string_width.fish
+++ functions/_pure_string_width.fish
@@ -5,5 +5,5 @@ function _pure_string_width \
     set --local empty ''
     set --local raw_prompt (string replace --all --regex '\e\[[^m]*m' $empty $prompt)
 
-    string length $raw_prompt
+    string length -- $raw_prompt
 end
