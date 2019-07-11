4aeacec4ab03e31bbeb37131b73c7f1be37b2eea
Edouard Lopez <contact@edouard-lopez.com>
2019-07-11 10:19:00 +0200

    fix variable testing
    fixes #181

--- functions/_pure_prompt_vimode.fish
+++ functions/_pure_prompt_vimode.fish
@@ -1,5 +1,5 @@
 function _pure_prompt_vimode
-    if test ! $pure_reverse_prompt_symbol_in_vimode
+    if test $pure_reverse_prompt_symbol_in_vimode = false
       echo (fish_default_mode_prompt)
     end
 end
--- tests/_pure_prompt_vimode.test.fish
+++ tests/_pure_prompt_vimode.test.fish
@@ -3,11 +3,18 @@ source $current_dirname/../functions/_pure_prompt_vimode.fish
 set --local empty ''
 
 @test "_pure_prompt_vimode: hides vimode prompt by default" (
-    _pure_prompt_vimode
+     echo (_pure_prompt_vimode)
 ) = $empty
 
-@test "_pure_prompt_vimode: shows default vimode prompt" (
+@test "_pure_prompt_vimode: show reverse prompt symbol when enable" (
+    set pure_reverse_prompt_symbol_in_vimode true
+
+    echo (_pure_prompt_vimode)
+) = $empty
+
+@test "_pure_prompt_vimode: show vi_mode when reverse prompt symbol is disable" (
     set fish_key_bindings fish_vi_key_bindings
+    set pure_reverse_prompt_symbol_in_vimode false
 
     _pure_prompt_vimode
     set fish_key_bindings fish_default_key_bindings
