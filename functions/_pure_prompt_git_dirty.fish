28fe8a5f18a33a5d87c30f7bbdd10087a234b16f
Duc Hoa, Nguyen <hoameomu@gmail.com>
2019-09-06 16:34:14 +0800

    Speed up git dirty
    Idea and implementation is from https://github.com/oh-my-fish/oh-my-fish/pull/706
    Modified to ignore submodules
    
    git commands that can exit early.
    In large repos, this can be faster by a factor of 15 or so.

--- functions/_pure_prompt_git_dirty.fish
+++ functions/_pure_prompt_git_dirty.fish
@@ -2,7 +2,10 @@ function _pure_prompt_git_dirty
     set --local git_dirty_symbol
     set --local git_dirty_color
 
-    set --local is_git_dirty (command git status --porcelain --ignore-submodules 2>/dev/null)
+    set --local is_git_dirty (
+        not command git diff-index --ignore-submodules --cached --quiet HEAD -- >/dev/null 2>&1
+        or not command git diff --ignore-submodules --no-ext-diff --quiet --exit-code >/dev/null 2>&1
+    )
     if test -n "$is_git_dirty"  # untracked or un-commited files
         set git_dirty_symbol "$pure_symbol_git_dirty"
         set git_dirty_color "$pure_color_git_dirty"
