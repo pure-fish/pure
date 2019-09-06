d6031652c911568a803a4984e370f5f651cdb5ef
Duc Hoa, Nguyen <hoameomu@gmail.com>
2019-09-06 17:37:50 +0800

    Fix conditions for prompt git dirty

--- functions/_pure_prompt_git_dirty.fish
+++ functions/_pure_prompt_git_dirty.fish
@@ -3,8 +3,11 @@ function _pure_prompt_git_dirty
     set --local git_dirty_color
 
     set --local is_git_dirty (
+        # The first checks for staged changes, the second for unstaged ones.
+        # We put them in this order because checking staged changes is *fast*.
         not command git diff-index --ignore-submodules --cached --quiet HEAD -- >/dev/null 2>&1
         or not command git diff --ignore-submodules --no-ext-diff --quiet --exit-code >/dev/null 2>&1
+        and echo "true"
     )
     if test -n "$is_git_dirty"  # untracked or un-commited files
         set git_dirty_symbol "$pure_symbol_git_dirty"
