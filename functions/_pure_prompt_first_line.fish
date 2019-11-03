07a222c88b602b5c89020caac9837a577ef3b522
schrodincat <schrodinger_cat@me.com>
2019-11-03 20:34:50 +0300

    Refactor `_pure_prompt_first_line`

--- functions/_pure_prompt_first_line.fish
+++ functions/_pure_prompt_first_line.fish
@@ -5,10 +5,13 @@ function _pure_prompt_first_line \
         return 1
     end
 
+    set --local prompt_ssh (_pure_prompt_ssh)
+    set --local prompt_git (_pure_prompt_git)
+    set --local prompt_command_duration (_pure_prompt_command_duration)
     set --local prompt (_pure_print_prompt \
-                            (_pure_prompt_ssh) \
-                            (_pure_prompt_git) \
-                            (_pure_prompt_command_duration)
+                            $prompt_ssh \
+                            $prompt_git \
+                            $prompt_command_duration
                         )
     set --local prompt_width (_pure_string_width $prompt)
     set --local current_folder (_pure_prompt_current_folder $prompt_width)
@@ -16,16 +19,16 @@ function _pure_prompt_first_line \
     set --local prompt_components
     if test $pure_begin_prompt_with_current_directory = true
         set prompt_components \
-                (_pure_prompt_current_folder $prompt_width) \
-                (_pure_prompt_git) \
-                (_pure_prompt_ssh) \
-                (_pure_prompt_command_duration)
+                $current_folder \
+                $prompt_git \
+                $prompt_ssh \
+                $prompt_command_duration
     else
         set prompt_components \
-                (_pure_prompt_ssh) \
-                (_pure_prompt_current_folder $prompt_width) \
-                (_pure_prompt_git) \
-                (_pure_prompt_command_duration)
+                $prompt_ssh \
+                $current_folder \
+                $prompt_git \
+                $prompt_command_duration
     end
 
     echo (_pure_print_prompt $prompt_components)
