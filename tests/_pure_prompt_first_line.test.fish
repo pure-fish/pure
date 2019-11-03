32a85c61936650e76de1919a136b394ae2519faf
schrodincat <schrodinger_cat@me.com>
2019-11-03 20:34:16 +0300

    Allow option to disable git info in prompt

--- tests/_pure_prompt_first_line.test.fish
+++ tests/_pure_prompt_first_line.test.fish
@@ -49,6 +49,7 @@ end
 ) = 1
 
 @test "_pure_prompt_first_line: print current directory, git, user@hostname (ssh-only), command duration" (
+    set pure_enable_git true
     set pure_begin_prompt_with_current_directory true
     _pure_prompt_first_line
 
@@ -56,6 +57,7 @@ end
 ) = '/tmp/test master user@hostname 1s'
 
 @test "_pure_prompt_first_line: print user@hostname (ssh-only), current directory, git, command duration" (
+    set pure_enable_git true
     set pure_begin_prompt_with_current_directory false
     _pure_prompt_first_line
 
