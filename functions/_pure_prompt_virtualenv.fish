aabcd000ac9905dd01c556b981b79fac52c44844
Shane Segal <shane.segal95@gmail.com>
2019-11-15 15:14:59 -0500

    Add test for virtualenvs created through conda

--- functions/_pure_prompt_virtualenv.fish
+++ functions/_pure_prompt_virtualenv.fish
@@ -4,5 +4,10 @@ function _pure_prompt_virtualenv --description "Display virtualenv directory"
         set --local virtualenv_color (_pure_set_color $pure_color_virtualenv)
 
         echo "$virtualenv_color$virtualenv"
+    else if test -n "$CONDA_DEFAULT_ENV"
+        set --local virtualenv (basename "$CONDA_DEFAULT_ENV")
+        set --local virtualenv_color "$pure_color_virtualenv"
+
+        echo "$virtualenv_color$virtualenv"
     end
 end
