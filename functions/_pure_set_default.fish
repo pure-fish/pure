5d2487fa28a889ba8d3b7ba533ecceb87b77275b
schrodincat <schrodinger_cat@me.com>
2019-12-23 19:24:40 +0300

    Fix empty default values for non-interactive terminals

--- functions/_pure_set_default.fish
+++ functions/_pure_set_default.fish
@@ -2,7 +2,7 @@ function _pure_set_default \
     --description 'Set default value for configuration variable' \
     --argument-names var default
 
-    if not set -q $var
+    if not set -q --universal $var; or test -z $$var
         set --universal $var $default
     end
 end
