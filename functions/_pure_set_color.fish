d66163b945966aa958601d9565efefd89b5c642a
schrodincat <schrodinger_cat@me.com>
2019-12-24 20:10:46 +0300

    Read colors as fish colors

--- functions/_pure_set_color.fish
+++ functions/_pure_set_color.fish
@@ -2,10 +2,11 @@ function _pure_set_color \
     --description 'Set color' \
     --argument-names var default
 
-    set --local result $default
+    set --local color $default
     if set -q $var
-        set result $$var
+        set color $$var
     end
 
+    set --local result (set_color $color)
     echo "$result"
 end
