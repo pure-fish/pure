39db4bab2e89f9e8dee643b56c6eebbcc4a57150
schrodincat <schrodinger_cat@me.com>
2019-12-24 21:45:33 +0300

    Remove a workaround for https://github.com/rafaelrinaldi/pure/issues/200

--- functions/_pure_set_default.fish
+++ functions/_pure_set_default.fish
@@ -2,7 +2,7 @@ function _pure_set_default \
     --description 'Set default value for configuration variable' \
     --argument-names var default
 
-    if not set -q --universal $var; or test -z $$var
+    if not set -q --universal $var
         set --universal $var $default
     end
 end
