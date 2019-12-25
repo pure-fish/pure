95cf875857d090eaec41e9b893695f5a2a15ac79
schrodincat <schrodinger_cat@me.com>
2019-12-25 22:26:58 +0300

    Read escape sequences from color variables for backwards compatibility

--- functions/_pure_set_color.fish
+++ functions/_pure_set_color.fish
@@ -3,10 +3,16 @@ function _pure_set_color \
     --argument-names var
 
     set --local color $var
-    if set -q $var
-        set color $$var
+    # Backwards compatibility for colors defined as control sequencies instead of fish colors
+    if not string match --quiet --all --regex '\e\[[^m]*m' $color[1]
+        and set -q $color
+            set color $$var
     end
 
-    set --local result (set_color $color)
+    set --local result $color
+    if not string match --quiet --all --regex '\e\[[^m]*m' $result[1]
+        set result (set_color $color)
+    end
+
     echo "$result"
 end
