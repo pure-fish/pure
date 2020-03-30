535a185ebb701361b1a6a4e83c4da50029da7d1a
Alexis Lahouze <alexis@lahouze.org>
2020-03-30 12:23:27 +0200

    Display user and hostname if in sudo session too.

--- functions/_pure_prompt_ssh.fish
+++ functions/_pure_prompt_ssh.fish
@@ -1,5 +1,5 @@
 function _pure_prompt_ssh
-    if test "$SSH_CONNECTION" != ""
+    if test "$SSH_CONNECTION" != "" -o "$SUDO_USER" != ""
         echo (_pure_prompt_ssh_user)(_pure_prompt_ssh_separator)(_pure_prompt_ssh_host)
     end
 end
