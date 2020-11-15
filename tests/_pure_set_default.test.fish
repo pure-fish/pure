06231d8543ed6f11f9db6bb8dd65c57d21224cb9
Edouard Lopez <contact@edouard-lopez.com>
2020-11-16 00:09:13 +0100

    add tests for _pure_set_default

--- tests/_pure_set_default.test.fish
+++ tests/_pure_set_default.test.fish
@@ -1,12 +1,32 @@
 source $current_dirname/../functions/_pure_set_default.fish
 
+set --local empty ''
+set --local fail 1
+
+function teardown
+    set --universal --erase my_var
+    set --global --erase my_var
+end
+
 @test "_pure_set_default: set my_var default value" (
     _pure_set_default my_var 'default_value'
     echo $my_var
 ) = 'default_value'
 
-@test "_pure_set_default: skip setting value if default already exists" (
+@test "_pure_set_default: skip setting value if default already exists at universal scope" (
     _pure_set_default my_var 'default_value'
+    _pure_set_default my_var 'another_value'
+    echo $my_var
+) = 'default_value'
+
+@test "_pure_set_default: overwrite UNIVERSAL empty value to make sure the user won't have empty colors." (
+    _pure_set_default my_var $empty
+    _pure_set_default my_var 'default_value'
+    echo $my_var
+) = 'default_value'
+
+@test "_pure_set_default: skip setting value if default already exists at global scope" (
+    set --global my_var 'default_value'
     _pure_set_default my_var 'another_value'
     echo $my_var
 ) = 'default_value'
