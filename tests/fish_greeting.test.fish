74841ee630bec27770c4d9774e005ec305d18f5f
Edouard Lopez <contact@edouard-lopez.com>
2020-11-19 12:48:13 +0100

    fix fish_greeting test

--- tests/fish_greeting.test.fish
+++ tests/fish_greeting.test.fish
@@ -1,6 +1,7 @@
 source $current_dirname/../functions/fish_greeting.fish
 
 set --local succeed 0
+function _pure_check_for_new_release; echo ''; end
 
 @test "fish_greeting: succeed" (
     fish_greeting
