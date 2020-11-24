606a71c0d9100bdb99d1e2b6a06b14801181ad5d
Edouard Lopez <contact@edouard-lopez.com>
2020-11-24 12:15:10 +0100

    merge all install method in same file to prevent race condition

--- tests/pure_tools_installer.test.fish
+++ tests/pure_tools_installer.test.fish
@@ -212,3 +212,67 @@ if test $USER = 'nemo'
         fish -c "fish_prompt" | grep -c '>'
     ) = $IS_PRESENT
 end
+
+
+
+
+
+
+
+if test $USER = 'nemo'
+    @test "installation methods: manually (with unpublished installer)" (
+        source tools/installer.fish
+
+        and install_pure >/dev/null
+        source $PURE_INSTALL_DIR/conf.d/*
+
+        fish -c 'fish_prompt | grep -c "❯"'
+    ) = $succeed
+end
+
+# if test $USER = 'nemo'
+#     @test "installation methods: manually (with published installer)" (
+#         curl git.io/pure-fish --output /tmp/installer.fish --location --silent
+#         and source /tmp/installer.fish
+#         and install_pure
+
+#         fish -c 'fish_prompt | grep -c "❯"'
+#     ) = $succeed
+# end
+
+# if test $USER = 'nemo'
+#     @test "installation methods: with fisher" (
+#         fisher install rafaelrinaldi/pure >/dev/null
+
+#         fish -c 'fish_prompt | grep -c "❯"'
+#     ) = $succeed
+# end
+
+# if test $USER = 'nemo'
+#     @test "installation methods: with OMF (Oh-My-Fish!)" (
+#         rm --recursive --force $HOME/.local/share/omf $HOME/.config/omf/
+
+#         curl -L https://get.oh-my.fish > install
+#         and fish install --noninteractive >/dev/null
+
+#         set --global OMF_PURE_PATH $HOME/.local/share/omf/themes/pure
+#         fish -c "omf install pure; \
+#                 ln -sf $OMF_PURE_PATH/functions/*.fish $HOME/.config/fish/functions/; \
+#                 ln -sf $OMF_PURE_PATH/conf.d/* $HOME/.config/fish/conf.d/" > /dev/null
+#         fish -c "fish_prompt" | grep -c '❯'
+#     ) = $succeed
+# end
+
+# if test $USER = 'nemo'
+#     @test "installation methods: with Fundle" (
+#         rm --recursive --force $HOME/.config/fish/fundle/
+#         mkdir -p $HOME/.config/fish/functions
+#         curl https://git.io/fundle --output $HOME/.config/fish/functions/fundle.fish --location --silent >/dev/null
+
+#         fundle plugin rafaelrinaldi/pure >/dev/null
+#         fundle install >/dev/null
+#         cp $HOME/.config/fish/fundle/rafaelrinaldi/pure/{,functions/}fish_prompt.fish
+
+#         fish -c 'fish_prompt | grep -c "❯"'
+#     ) = 1
+# end
