72d37757bac1d4a2ee4364df537f65f4d3a21a2f
Edouard Lopez <contact@edouard-lopez.com>
2020-11-28 22:15:48 +0100

    remove test related to missing program name in fish_title

--- tests/fish_title.test.fish
+++ tests/fish_title.test.fish
@@ -10,14 +10,6 @@ function setup
     function _pure_parse_directory; echo /tmp/current/directory; end
 end
 
-if fish_version_below '3.0.0'
-    @mesg (print_fish_version_below '3.0.0')
-    @test "fish_title: contains current directory and previous command" (
-        set --global pure_symbol_title_bar_separator '—'
-        fish_title 'last-command'
-    ) = "directory: last-command — "
-end
-
 if fish_version_at_least '3.0.0'
     @mesg (print_fish_version_at_least '3.0.0')
     @test "fish_title: contains current directory and previous command" (
@@ -26,26 +18,11 @@ if fish_version_at_least '3.0.0'
     ) = "directory: last-command — fish"
 end
 
-if fish_version_below '3.0.0'
-    @mesg (print_fish_version_below '3.0.0')
-    @test "fish_title: contains current directory with *empty* a previous command" (
-        fish_title ''
-    ) = "/tmp/current/directory — "
-end
-
 if fish_version_at_least '3.0.0'
     @mesg (print_fish_version_at_least '3.0.0')
     @test "fish_title: contains current directory with an *empty* previous command" (
         fish_title ''
     ) = "/tmp/current/directory — fish"
-end
-
-if fish_version_below '3.0.0'
-    @mesg (print_fish_version_below '3.0.0')
-    @test "fish_title: contains current path without a previous command" (
-        set --global pure_symbol_title_bar_separator '—'
-        fish_title
-    ) = "/tmp/current/directory — "
 end
 
 if fish_version_at_least '3.0.0'
