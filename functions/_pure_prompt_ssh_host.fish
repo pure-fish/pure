521788840cae7abc4a737c10bdc9b79e12b45eeb
Edouard Lopez <contact@edouard-lopez.com>
2020-11-28 22:24:16 +0100

    remove code related thanks to native `$hostname` variable

--- functions/_pure_prompt_ssh_host.fish
+++ functions/_pure_prompt_ssh_host.fish
@@ -1,6 +1,4 @@
 function _pure_prompt_ssh_host
-    set --query --global hostname
-    or set --local hostname (hostname -s) # current host name compatible busybox
     set --local hostname_color (_pure_set_color $pure_color_ssh_hostname)
 
     echo "$hostname_color$hostname"
