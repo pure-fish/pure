ce2ac8850a4c0549b7c73bc9c19f3e974f47a49e
Edouard Lopez <contact@edouard-lopez.com>
2020-12-03 21:42:22 +0100

    add to prompt

--- functions/_pure_prompt_git.fish
+++ functions/_pure_prompt_git.fish
@@ -8,7 +8,7 @@ function _pure_prompt_git \
     set --local is_git_repository (command git rev-parse --is-inside-work-tree 2>/dev/null)
 
     if test -n "$is_git_repository"
-        set --local git_prompt (_pure_prompt_git_branch)(_pure_prompt_git_dirty)
+        set --local git_prompt (_pure_prompt_git_branch)(_pure_prompt_git_dirty)(_pure_prompt_git_stash)
         set --local git_pending_commits (_pure_prompt_git_pending_commits)
 
         if test (_pure_string_width $git_pending_commits) -ne 0
