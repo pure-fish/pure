1addd519dd0093d59dc02a0736303c00f8e31c20
Edouard Lopez <contact@edouard-lopez.com>
2020-12-04 11:25:29 +0100

    test CONDA_DEFAULT_ENV detection

--- tests/_pure_prompt_virtualenv.test.fish
+++ tests/_pure_prompt_virtualenv.test.fish
@@ -12,8 +12,24 @@ source $current_dirname/../functions/_pure_set_color.fish
 
 @test "_pure_prompt_virtualenv: displays virtualenv directory prompt" (
     set VIRTUAL_ENV /home/test/fake/project/
-    set --global pure_color_virtualenv grey
+    set pure_color_virtualenv $EMPTY
 
     _pure_prompt_virtualenv
     set --erase --global VIRTUAL_ENV
-) = (set_color grey)'project'
+) = 'project'
+
+@test "_pure_prompt_virtualenv: hide Conda virtualenv prompt when not activated" (
+    set --erase CONDA_DEFAULT_ENV
+
+    _pure_prompt_virtualenv
+
+) $status -eq $SUCCESS
+
+@test "_pure_prompt_virtualenv: displays Conda virtualenv directory prompt" (
+    set CONDA_DEFAULT_ENV /home/test/fake/project/
+    set pure_color_virtualenv $EMPTY
+
+    _pure_prompt_virtualenv
+    set --erase --global CONDA_DEFAULT_ENV
+) = 'project'
+
