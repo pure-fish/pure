a4f050bce5ddd2a3d4c8694a43aca1e8fff375cd
Edouard Lopez <contact@edouard-lopez.com>
2020-12-11 11:09:26 +0100

    use long format options

--- functions/_pure_format_time.fish
+++ functions/_pure_format_time.fish
@@ -5,23 +5,23 @@ function _pure_format_time \
     --argument-names milliseconds threshold show_subsecond
 
     test $milliseconds -lt 0; and return $fail
-    test $milliseconds -lt (math -s0 "$threshold * 1000"); and echo; and return 0
+    test $milliseconds -lt (math --scale=0 "$threshold * 1000"); and echo; and return $SUCCESS
 
     set --local time
 
-    set --local days (math -s0 "$milliseconds / 86400000")
-    test $days -gt 0; and set time $time (printf "%sd" $days)
+    set --local days (math --scale=0 "$milliseconds / 86400000")
+    test $days -gt 0; and set --append time (printf "%sd" $days)
 
-    set --local hours (math -s0 "$milliseconds / 3600000 % 24")
-    test $hours -gt 0; and set time $time (printf "%sh" $hours)
+    set --local hours (math --scale=0 "$milliseconds / 3600000 % 24")
+    test $hours -gt 0; and set --append time (printf "%sh" $hours)
 
-    set --local minutes (math -s0 "$milliseconds / 60000 % 60")
-    test $minutes -gt 0; and set time $time (printf "%sm" $minutes)
+    set --local minutes (math --scale=0 "$milliseconds / 60000 % 60")
+    test $minutes -gt 0; and set --append time (printf "%sm" $minutes)
 
-    set --local seconds (math -s0 "$milliseconds / 1000 % 60")
+    set --local seconds (math --scale=0 "$milliseconds / 1000 % 60")
     if test $show_subsecond = true
         if test $milliseconds -gt 0
-            set --local subsec (string sub -s -3 -l 2 00$milliseconds | string trim -r -c 0)
+            set --local subsec (string sub --start -3 --length 2 00$milliseconds | string trim --right --chars 0)
             set time $time $seconds(test -n "$subsec"; and echo '.'$subsec; or echo)'s'
         end
     else
