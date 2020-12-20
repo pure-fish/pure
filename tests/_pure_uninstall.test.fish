854b57ea56d2ebec336ddb37c46cb38b74e9b6d5
mattmc3 <mattmc3@gmail.com>
2020-12-20 12:23:55 -0500

    Fix broken CI test for _pure_uninstall function

--- tests/_pure_uninstall.test.fish
+++ tests/_pure_uninstall.test.fish
@@ -2,6 +2,6 @@ source $current_dirname/fixtures/constants.fish
 
 @test "init: source uninstall handler"  (
     functions --erase _pure_uninstall
-    source $current_dirname/../functions/_pure_uninstall.fish
+    source $current_dirname/../conf.d/_pure_init.fish
     functions --query _pure_uninstall
 ) $status -eq $SUCCESS
