6fb3723f8418139440b0475034753f36f3ecfcb0
Edouard Lopez <contact@edouard-lopez.com>
2020-12-21 15:58:48 +0100

    add prefix_root_prompt feature
    fixes #117

---
+++ tests/_pure_prefix_root_prompt.test.fish
@@ -0,0 +1,20 @@
+source $current_dirname/fixtures/constants.fish
+source $current_dirname/../functions/_pure_set_color.fish
+source $current_dirname/../functions/_pure_prefix_root_prompt.fish
+
+set pure_symbol_prefix_root_prompt '#'
+
+@test "_pure_prefix_root_prompt: is empty for unprivileged user" (
+    set pure_show_prefix_root_prompt true
+    function whoami; echo 'nemo'; end # mock
+
+    _pure_prefix_root_prompt
+) = $EMPTY
+
+@test "_pure_prefix_root_prompt: is shown for `root` user" (
+    set pure_show_prefix_root_prompt true
+    set pure_color_prefix_root_prompt red
+    function whoami; echo 'root'; end # mock
+
+    _pure_prefix_root_prompt
+) = (set_color red)"#$SPACE"
--- tests/fixtures/constants.fish
+++ tests/fixtures/constants.fish
@@ -3,3 +3,4 @@ set --global FAILURE 1
 set --global IS_PRESENT 1 # when using grep
 set --global EMPTY ''
 set --global NONE 0
+set --global SPACE ' '
