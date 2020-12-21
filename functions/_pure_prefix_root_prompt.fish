935a55faf3ad6fc563d560dfdb95e9356074c8f0
Edouard Lopez <contact@edouard-lopez.com>
2020-12-21 15:58:48 +0100

    add prefix_root_prompt feature
    fixes #117

---
+++ functions/_pure_prefix_root_prompt.fish
@@ -0,0 +1,12 @@
+function _pure_prefix_root_prompt
+    set --local username (whoami) # current user name
+    set --local prefix_root_prompt ''
+    set --local space ' '
+
+    if test $pure_show_prefix_root_prompt = true -a "$username" = "root"
+        set --local prefix_color (_pure_set_color $pure_color_prefix_root_prompt)
+        set prefix_root_prompt "$prefix_color$pure_symbol_prefix_root_prompt$space"
+    end
+
+    echo "$prefix_root_prompt"
+end
