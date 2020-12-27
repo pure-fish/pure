e7a971c5ab746b82927e930260791a448338abc1
Edouard Lopez <contact@edouard-lopez.com>
2020-12-28 00:41:41 +0100

    use `date` short format option for MacOS compatibility

--- tests/_pure_prompt_system_time.test.fish
+++ tests/_pure_prompt_system_time.test.fish
@@ -12,7 +12,11 @@ function _pure_set_color; echo $EMPTY; end # drop coloring during test
 
 @test "_pure_prompt_system_time: displays system time when enable" (
     set --global pure_show_system_time true
-    function date; /bin/date --date='10:01:29' '+%T'; end
+    function date
+        test (uname) = Darwin;
+        and command date -j -f "%H:%M:%S" '10:01:29' '+%T'; # MacOS
+        or command date -d '10:01:29' '+%T'; # Linux
+    end
 
     _pure_prompt_system_time
 ) = '10:01:29'
