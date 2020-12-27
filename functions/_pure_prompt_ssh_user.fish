8342082a2b67c40c375fa688a442a7d4e2372e14
Edouard Lopez <contact@edouard-lopez.com>
2020-12-27 20:47:24 +0100

    use short format option for compatibility with macOS
    fixes #258

--- functions/_pure_prompt_ssh_user.fish
+++ functions/_pure_prompt_ssh_user.fish
@@ -1,5 +1,5 @@
 function _pure_prompt_ssh_user
-    set --local username (id --user --name) # current user name
+    set --local username (id -u -n) # current user name
     set --local username_color (_pure_set_color $pure_color_ssh_user_normal) # default color
 
     if test "$username" = "root"
