874adf4c1a856ff35d290d5c96071cb217f6a46b
Edouard Lopez <contact@edouard-lopez.com>
2020-12-28 20:13:44 +0100

    extract `_pure_print_prompt_rows` function

---
+++ tests/_pure_print_prompt_rows.test.fish
@@ -0,0 +1,17 @@
+source $current_dirname/fixtures/constants.fish
+# source $current_dirname/../functions/_pure_prompt_first_line.fish
+source $current_dirname/../functions/_pure_print_prompt_rows.fish
+
+function setup
+    function _pure_prompt_first_line; echo $EMPTY; end
+end
+
+@test "_pure_print_prompt_rows: end with newline by default" (
+    _pure_print_prompt_rows | wc -l
+) = 1
+
+@test "_pure_print_prompt_rows: end WITHOUT newline when compact-prompt is enable" (
+    set pure_enable_compact_prompt true
+
+    _pure_print_prompt_rows | wc -l
+) = 0
