d34367ef4f8099d036e4ee223a2e96e6e5f4849b
Edouard Lopez <contact@edouard-lopez.com>
2020-12-29 22:47:51 +0100

    revamp `_pure_set_default`

--- functions/_pure_set_default.fish
+++ functions/_pure_set_default.fish
@@ -2,7 +2,10 @@ function _pure_set_default \
     --description 'Set default value for configuration variable' \
     --argument-names var default
 
-    if  not set --query --universal $var; or test -z $$var; and not set --query --global $var
-        set --universal $var $default
+    set is_available_universally (not set --query --universal $var; or test -z $$var; echo $status)
+    set is_available_globally (not set --query --global $var; or test -z $$var; echo $status)
+
+    if test $is_available_universally -eq 0 -a $is_available_globally -eq 0
+        set --universal $var "$default"
     end
 end
