4aba62d4b4418666e969ed00922d44a492da118d
Edouard Lopez <contact@edouard-lopez.com>
2020-12-29 15:39:36 +0100

    test pure_show_jobs colorization separatly and reduce test duration

--- tests/_pure_prompt_jobs.test.fish
+++ tests/_pure_prompt_jobs.test.fish
@@ -2,15 +2,24 @@ source $current_dirname/fixtures/constants.fish
 source $current_dirname/../functions/_pure_prompt_jobs.fish
 source $current_dirname/../functions/_pure_set_color.fish
 
+set JOB_DURATION 1.5
 
 @test "_pure_prompt_jobs: no jobs indicator when there are no jobs" (
     _pure_prompt_jobs
 ) $status -eq $SUCCESS
 
 @test "_pure_prompt_jobs: displays number of jobs in prompt" (
+    set --global pure_show_jobs true
+    sleep $JOB_DURATION &
+    _pure_prompt_jobs
+    kill (jobs -p)
+) = '[1]'
+
+@test "_pure_prompt_jobs: colorize jobs" (
     set --global pure_color_jobs grey
     set --global pure_show_jobs true
-    sleep 5 &
+    sleep $JOB_DURATION &
+
     _pure_prompt_jobs
     kill (jobs -p)
 ) = (set_color grey)'[1]'
