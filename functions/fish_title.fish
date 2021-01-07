5e8004ab9afca027efc79feec86ef1d987dfee67
Edouard Lopez <contact@edouard-lopez.com>
2021-01-07 20:56:42 +0100

    use long-format option `--regex`

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -2,7 +2,7 @@ function fish_title \
     --description "Set title to current folder and shell name" \
     --argument-names last_command
 
-    set --local basename (string replace -r '^.*/' '' -- $PWD)
+    set --local basename (string replace --regex '^.*/' '' -- $PWD)
     set --local current_folder (_pure_parse_directory)
     set --local current_command (status current-command 2>/dev/null; or echo $_)
 
