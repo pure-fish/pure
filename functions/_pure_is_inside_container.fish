971fbe35997a8133aea70b246ae6eebc9519031e
Edouard Lopez <contact@edouard-lopez.com>
2021-01-09 22:13:09 +0100

    add _pure_is_inside_container
    see #241

---
+++ functions/_pure_is_inside_container.fish
@@ -0,0 +1,14 @@
+function _pure_is_inside_container \
+    --argument-names cgroup_namespace
+    set --query cgroup_namespace[1]; or set cgroup_namespace /proc/1/cgroup
+
+    begin
+        test -r $cgroup_namespace
+        and string match \
+                --quiet \
+                --entire \
+                --regex '(lxc|docker)' \
+            <$cgroup_namespace
+    end
+    or test $container = "lxc"
+end
