913058fe6d21a1a75046eef6a8ff6ff7b4f0e196
Edouard Lopez <contact@edouard-lopez.com>
2021-01-13 16:38:10 +0100

    remove import for version-compare in test

--- tests/fish_title.test.fish
+++ tests/fish_title.test.fish
@@ -1,6 +1,5 @@
 source $current_dirname/fixtures/constants.fish
 source $current_dirname/../functions/fish_title.fish
-source $current_dirname/../tools/versions-compare.fish
 @mesg (_print_filename $current_filename)
 
 
