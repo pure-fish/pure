e69874d9eefd7c083b75e836ef660f042b4dc73f
Edouard Lopez <contact@edouard-lopez.com>
2021-01-13 16:28:06 +0100

    show user@host when running in a container

--- functions/_pure_prompt_first_line.fish
+++ functions/_pure_prompt_first_line.fish
@@ -8,10 +8,12 @@ function _pure_prompt_first_line \
     end
 
     set --local prompt_ssh (_pure_prompt_ssh)
+    set --local prompt_container (_pure_prompt_container)
     set --local prompt_git (_pure_prompt_git)
     set --local prompt_command_duration (_pure_prompt_command_duration)
     set --local prompt (_pure_print_prompt \
                             $prompt_ssh \
+                            $prompt_container \
                             $prompt_git \
                             $prompt_command_duration
                         )
@@ -24,10 +26,12 @@ function _pure_prompt_first_line \
                 $current_folder \
                 $prompt_git \
                 $prompt_ssh \
+                $prompt_container \
                 $prompt_command_duration
     else
         set prompt_components \
                 $prompt_ssh \
+                $prompt_container \
                 $current_folder \
                 $prompt_git \
                 $prompt_command_duration
