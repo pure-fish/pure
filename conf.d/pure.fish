69e9a074125ad853aae244ce2aabc33811b99970
Edouard Lopez <contact@edouard-lopez.com>
2021-03-03 10:05:01 +0100

    bump version 4.1.1
    see #278

--- conf.d/pure.fish
+++ conf.d/pure.fish
@@ -1,4 +1,4 @@
-set --global pure_version 4.1.0 # used for bug report
+set --global pure_version 4.1.1 # For bug report and tag-after-merge workflow
 
 # Base colors
 _pure_set_default pure_color_primary blue
