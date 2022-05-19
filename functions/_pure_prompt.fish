8c054195986a84dd6d3250acf3084460f26b7165
Édouard Lopez <edouard.lopez@manomano.com>
2022-05-19 17:22:51 +0200

    chore: lint

--- functions/_pure_prompt.fish
+++ functions/_pure_prompt.fish
@@ -9,18 +9,19 @@ function _pure_prompt \
     set --local system_time (_pure_prompt_system_time)
     set --local root_prefix (_pure_prefix_root_prompt)
     set --local space
+
     if _pure_is_single_line_prompt
         set space ' '
     end
 
     echo (\
-        _pure_print_prompt \
-        $system_time \
-        $root_prefix \
-        $jobs \
-        $virtualenv \
-        $vimode_indicator \
-        $space \
-        $pure_symbol \
+            _pure_print_prompt \
+                $system_time \
+                $root_prefix \
+                $jobs \
+                $virtualenv \
+                $vimode_indicator \
+                $space \
+                $pure_symbol \
     )
 end
