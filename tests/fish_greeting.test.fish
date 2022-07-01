0e0cb4d377e6ad5da55dd6ae69bf322ce9aa2e2e
Edouard Lopez <contact@edouard-lopez.com>
2022-07-01 15:30:33 +0200

    test: fix fish_greeting.test.fish

--- tests/fish_greeting.test.fish
+++ tests/fish_greeting.test.fish
@@ -3,12 +3,22 @@ source (dirname (status filename))/../functions/fish_greeting.fish
 @echo (_print_filename (status filename))
 
 
-function _pure_check_for_new_release; echo ''; end
 
-@test "fish_greeting: succeed" (
+function before_each
+    _purge_configs
+    _disable_colors
+end
+
+function _pure_check_for_new_release --description "stub function"
+    return $SUCCESS
+end
+
+before_each
+@test "fish_greeting: succeeds" (
     fish_greeting
 ) $status -eq $SUCCESS
 
+before_each
 @test "fish_greeting: returns nothing" (
     fish_greeting
 ) -z
