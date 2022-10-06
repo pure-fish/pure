edb4f5680f4378c2d7495f0108079353d40ff019
Édouard Lopez <edouard.lopez@manomano.com>
2022-10-06 17:13:44 +0200

    fix(macos): use portable `sed command`
    see https://stackoverflow.com/questions/21242932/sed-i-may-not-be-used-with-stdin-on-mac-os-x#comment36378028_21243111

--- tools/migration-to-4.0.0.fish
+++ tools/migration-to-4.0.0.fish
@@ -10,7 +10,11 @@ end
 printf "%sMigrating configuration: $fish_user_config\n"
 
 printf (set_color cyan)"Renaming SSH variables\n"(set_color normal)
-sed -i 's/pure_color_ssh_hostname/pure_color_hostname/g' $fish_user_config #; grep -c 'pure_color_ssh_host' $fish_user_config
-sed -i 's/pure_color_ssh_separator/pure_color_at_sign/g' $fish_user_config #; grep -c 'pure_color_ssh_root' $fish_user_config
-sed -i 's/pure_color_ssh_user_normal/pure_color_username_normal/g' $fish_user_config #; grep -c 'pure_color_ssh_separator' $fish_user_config
-sed -i 's/pure_color_ssh_user_root/pure_color_username_root/g' $fish_user_config #; grep -c 'pure_color_ssh_username' $fish_user_config
+sed 's/pure_color_ssh_hostname/pure_color_hostname/g' $fish_user_config > $fish_user_config.new #; grep -c 'pure_color_ssh_host' $fish_user_config
+mv $fish_user_config.new $fish_user_config # for portability with MacOS
+sed 's/pure_color_ssh_separator/pure_color_at_sign/g' $fish_user_config > $fish_user_config.new #; grep -c 'pure_color_ssh_root' $fish_user_config
+mv $fish_user_config.new $fish_user_config # for portability with MacOS
+sed 's/pure_color_ssh_user_normal/pure_color_username_normal/g' $fish_user_config > $fish_user_config.new #; grep -c 'pure_color_ssh_separator' $fish_user_config
+mv $fish_user_config.new $fish_user_config # for portability with MacOS
+sed 's/pure_color_ssh_user_root/pure_color_username_root/g' $fish_user_config > $fish_user_config.new #; grep -c 'pure_color_ssh_username' $fish_user_config
+mv $fish_user_config.new $fish_user_config # for portability with MacOS
