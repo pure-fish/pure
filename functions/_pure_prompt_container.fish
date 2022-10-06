47a5496f9ac5d61939af5bef2e3940be64fc0c87
Édouard Lopez <edouard.lopez@manomano.com>
2022-10-06 17:02:22 +0200

    chore: cleanup _pure_prompt_container

--- functions/_pure_prompt_container.fish
+++ functions/_pure_prompt_container.fish
@@ -1,6 +1,4 @@
 function _pure_prompt_container
-    _pure_is_inside_container
-    echo _pure_is_inside_container $status
     if _pure_is_inside_container
         echo (_pure_user_at_host)
     end
