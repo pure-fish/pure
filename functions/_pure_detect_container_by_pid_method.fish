c3f88b66e20e8d3362b895978999bcc540781482
Édouard Lopez <edouard.lopez@manomano.com>
2022-10-06 16:02:28 +0200

    test: move MacOS check to pid_method and add missing imports on tests

--- functions/_pure_detect_container_by_pid_method.fish
+++ functions/_pure_detect_container_by_pid_method.fish
@@ -3,9 +3,15 @@ function _pure_detect_container_by_pid_method \
 
     set --query proc_sched[1]; or set proc_sched /proc/1/sched
 
-    head -n 1 $proc_sched \
-        | string match \
-        --quiet \
-        --invert \
-        --regex 'init|systemd'
+    set --local os_name (uname -s)
+    if test $os_name != "Darwin"
+        head -n 1 $proc_sched \
+            | string match \
+            --quiet \
+            --invert \
+            --regex 'init|systemd'
+    else
+        set --local failure 1
+        return $failure
+    end
 end
