0027f5b40b930e6aef20669233841412200e700c
Édouard Lopez <edouard.lopez@manomano.com>
2022-10-11 12:23:12 +0200

    feat(container): implement `pure_enable_container_detection` feature flag

--- functions/_pure_is_inside_container.fish
+++ functions/_pure_is_inside_container.fish
@@ -2,22 +2,25 @@ function _pure_is_inside_container \
     --argument-names cgroup_namespace
     set --query cgroup_namespace[1]; or set cgroup_namespace /proc/1/cgroup
 
-    set --local success 0
-    if test -n "$container"
-        return $success
-    end
-
-    set --local os_name (uname -s)
-    if test $os_name = Linux
-        if _pure_detect_container_by_cgroup_method $cgroup_namespace
+    if set --query pure_enable_container_detection; and test "$pure_enable_container_detection" = true
+        set --local success 0
+        if test -n "$container"
             return $success
         end
 
-        if _pure_detect_container_by_pid_method
-            return $success
-        end
-    end
+        set --local os_name (uname -s)
+        # echo $os_name
+        if test $os_name = Linux
+            if _pure_detect_container_by_cgroup_method $cgroup_namespace
+                return $success
+            end
 
-    set --local failure 1
-    return $failure
+            if _pure_detect_container_by_pid_method
+                return $success
+            end
+        end
+
+        set --local failure 1
+        return $failure
+    end
 end
