10315538b3e6dc86b0126f1f4fd080a096f54e59
Édouard Lopez <edouard.lopez@manomano.com>
2022-10-11 12:23:12 +0200

    feat(container): implement `pure_enable_container_detection` feature flag

--- tests/_pure_is_inside_container.test.fish
+++ tests/_pure_is_inside_container.test.fish
@@ -16,11 +16,14 @@ function setup
 end
 setup
 
-function before_each
+function cleanup_detection_methods
     functions --erase _pure_detect_container_by_pid_method
     functions --erase _pure_detect_container_by_cgroup_method
-    functions --erase uname
     set --global container ""
+end
+
+function cleanup_spy
+    functions --erase uname
 end
 
 function teardown
@@ -29,31 +32,56 @@ function teardown
         $cgroup_namespace
     set --erase cgroup_namespace
     set --erase namespace
+    set --erase called
 end
 
+@test "pure_enable_container_detection: feature is disabled" (
+    set --universal pure_enable_container_detection false
+    set --universal called "never"
+    function uname; set called once; echo "fake-os" ; end # spy
+
+    _pure_is_inside_container
+    echo $called
+) = never
+
+@test "pure_enable_container_detection: feature is enabled" (
+    set --universal pure_enable_container_detection true
+    set --universal called "never"
+    function uname; set called once; echo "fake-os" ; end # spy
+
+    _pure_is_inside_container
+    echo $called
+) = once
+
+cleanup_spy
 @test "_pure_is_inside_container: true for Github Action" (
+    set --universal pure_enable_container_detection true
+
     _pure_is_inside_container
 ) $status -eq $SUCCESS
 
 @test "_pure_is_inside_container: detect with $container variable" (
+    set --universal pure_enable_container_detection true
     set --global container "fake"
 
     _pure_is_inside_container
 ) $status -eq $SUCCESS
 
+cleanup_detection_methods
 if test (uname -s) = Linux
-    before_each
     @test "_pure_is_inside_container: detect with pid method" (
-    function _pure_detect_container_by_cgroup_method; false; end # spy
-    function _pure_detect_container_by_pid_method; echo "called: "(status function); end # spy
+        set --universal pure_enable_container_detection true
+        function _pure_detect_container_by_cgroup_method; false; end # spy
+        function _pure_detect_container_by_pid_method; echo "called: "(status function); end # spy
 
-    _pure_is_inside_container
+        _pure_is_inside_container
     ) = "called: _pure_detect_container_by_pid_method"
 end
 
+cleanup_detection_methods
 if test (uname -s) = Linux
-    before_each
     @test "_pure_is_inside_container: detect with cgroup method" (
+        set --universal pure_enable_container_detection true
         function _pure_detect_container_by_cgroup_method; echo "called: "(status function); end # spy
 
         _pure_is_inside_container
