487a27e027075bfb8a3ab7f9a1fbf0504ac89828
Édouard Lopez <edouard.lopez@manomano.com>
2022-10-14 13:08:37 +0200

    refactor: restrict container detection by pid only when required file exists
    fix https://github.com/pure-fish/pure/issues/301#issuecomment-1278554045

--- functions/_pure_detect_container_by_pid_method.fish
+++ functions/_pure_detect_container_by_pid_method.fish
@@ -1,11 +1,14 @@
 function _pure_detect_container_by_pid_method \
-    --description "Linux method to detect container using /proc. see https://stackoverflow.com/a/37015387/802365"
+    --description "Linux method to detect container using /proc. see https://stackoverflow.com/a/37015387/802365" \
+    --argument-names proc_sched
 
     set --query proc_sched[1]; or set proc_sched /proc/1/sched
 
-    head -n 1 $proc_sched \
-        | string match \
-        --quiet \
-        --invert \
-        --regex 'init|systemd'
+    if test -e $proc_sched
+        head -n 1 $proc_sched \
+            | string match \
+            --quiet \
+            --invert \
+            --regex 'init|systemd'
+    end
 end
