bedae60ff5708d097a2a217abfcc62775c4667ed
Edouard Lopez <contact@edouard-lopez.com>
2022-12-01 21:52:52 +0100

    chore(lint): re-format

--- tests/_pure.test.fish
+++ tests/_pure.test.fish
@@ -6,21 +6,22 @@ source (dirname (status filename))/../functions/_pure_set_default.fish
 function setup
     _purge_configs
     _disable_colors
-end; setup
+end
+setup
 
-@test "configure: pure_version"  (
+@test "configure: pure_version" (
     set --erase pure_version
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_version
 ) != $EMPTY
 
-@test "configure: pure_symbol_prompt"  (
+@test "configure: pure_symbol_prompt" (
     set --erase pure_symbol_prompt
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_prompt
 ) = "❯"
 
-@test "configure: pure_symbol_reverse_prompt"  (
+@test "configure: pure_symbol_reverse_prompt" (
     set --erase pure_symbol_reverse_prompt
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_reverse_prompt
@@ -32,223 +33,223 @@ end; setup
     echo $pure_reverse_prompt_symbol_in_vimode
 ) = true
 
-@test "configure: pure_enable_git"  (
+@test "configure: pure_enable_git" (
     set --erase pure_enable_git
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_enable_git
 ) = true
 
-@test "configure: pure_symbol_git_unpulled_commits"  (
+@test "configure: pure_symbol_git_unpulled_commits" (
     set --erase pure_symbol_git_unpulled_commits
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_git_unpulled_commits
 ) = "⇣"
 
-@test "configure: pure_symbol_git_unpushed_commits"  (
+@test "configure: pure_symbol_git_unpushed_commits" (
     set --erase pure_symbol_git_unpushed_commits
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_git_unpushed_commits
 ) = "⇡"
 
-@test "configure: pure_symbol_git_dirty"  (
+@test "configure: pure_symbol_git_dirty" (
     set --erase pure_symbol_git_dirty
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_git_dirty
 ) = "*"
 
-@test "configure: pure_symbol_git_stash"  (
+@test "configure: pure_symbol_git_stash" (
     set --erase pure_symbol_git_stash
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_git_stash
 ) = "≡"
 
-@test "configure: pure_symbol_title_bar_separator"  (
+@test "configure: pure_symbol_title_bar_separator" (
     set --erase pure_symbol_title_bar_separator
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_symbol_title_bar_separator
-) = "-"
+) = -
 
-@test "configure: pure_color_primary"  (
+@test "configure: pure_color_primary" (
     set --erase pure_color_primary
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_primary
 ) = blue
 
-@test "configure: pure_color_info"  (
+@test "configure: pure_color_info" (
     set --erase pure_color_info
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_info
 ) = cyan
 
-@test "configure: pure_color_mute"  (
+@test "configure: pure_color_mute" (
     set --erase pure_color_mute
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_mute
 ) = brblack
 
-@test "configure: pure_color_success"  (
+@test "configure: pure_color_success" (
     set --erase pure_color_success
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_success
 ) = magenta
 
-@test "configure: pure_color_normal"  (
+@test "configure: pure_color_normal" (
     set --erase pure_color_normal
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_normal
 ) = normal
 
-@test "configure: pure_color_danger"  (
+@test "configure: pure_color_danger" (
     set --erase pure_color_danger
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_danger
 ) = red
 
-@test "configure: pure_color_light"  (
+@test "configure: pure_color_light" (
     set --erase pure_color_light
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_light
 ) = white
 
-@test "configure: pure_color_warning"  (
+@test "configure: pure_color_warning" (
     set --erase pure_color_warning
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_warning
 ) = yellow
 
-@test "configure: pure_color_command_duration"  (
+@test "configure: pure_color_command_duration" (
     set --erase pure_color_command_duration
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_command_duration
 ) = pure_color_warning
 
-@test "configure: pure_color_current_directory"  (
+@test "configure: pure_color_current_directory" (
     set --erase pure_color_current_directory
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_current_directory
 ) = pure_color_primary
 
-@test "configure: pure_color_git_unpushed_commits"  (
+@test "configure: pure_color_git_unpushed_commits" (
     set --erase pure_color_git_unpushed_commits
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_git_unpushed_commits
 ) = pure_color_info
 
-@test "configure: pure_color_git_unpulled_commits"  (
+@test "configure: pure_color_git_unpulled_commits" (
     set --erase pure_color_git_unpulled_commits
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_git_unpulled_commits
 ) = pure_color_info
 
-@test "configure: pure_color_git_branch"  (
+@test "configure: pure_color_git_branch" (
     set --erase pure_color_git_branch
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_git_branch
 ) = pure_color_mute
 
-@test "configure: pure_color_git_dirty"  (
+@test "configure: pure_color_git_dirty" (
     set --erase pure_color_git_dirty
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_git_dirty
 ) = pure_color_mute
 
-@test "configure: pure_color_git_stash"  (
+@test "configure: pure_color_git_stash" (
     set --erase pure_color_git_stash
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_git_stash
 ) = pure_color_info
 
-@test "configure: pure_color_hostname"  (
+@test "configure: pure_color_hostname" (
     set --erase pure_color_hostname
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_hostname
 ) = pure_color_mute
 
-@test "configure: pure_color_at_sign"  (
+@test "configure: pure_color_at_sign" (
     set --erase pure_color_at_sign
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_at_sign
 ) = pure_color_mute
 
-@test "configure: pure_color_username_normal"  (
+@test "configure: pure_color_username_normal" (
     set --erase pure_color_username_normal
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_username_normal
 ) = pure_color_mute
 
-@test "configure: pure_color_username_root"  (
+@test "configure: pure_color_username_root" (
     set --erase pure_color_username_root
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_username_root
 ) = pure_color_light
 
-@test "configure: pure_color_prompt_on_error"  (
+@test "configure: pure_color_prompt_on_error" (
     set --erase pure_color_prompt_on_error
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_prompt_on_error
 ) = pure_color_danger
 
-@test "configure: pure_color_prompt_on_success"  (
+@test "configure: pure_color_prompt_on_success" (
     set --erase pure_color_prompt_on_success
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_prompt_on_success
 ) = pure_color_success
 
-@test "configure: pure_show_jobs"  (
+@test "configure: pure_show_jobs" (
     set --erase pure_show_jobs
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_show_jobs
 ) = false
 
-@test "configure: pure_color_jobs"  (
+@test "configure: pure_color_jobs" (
     set --erase pure_color_jobs
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_jobs
 ) = pure_color_normal
 
-@test "configure: pure_show_system_time"  (
+@test "configure: pure_show_system_time" (
     set --erase pure_show_system_time
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_show_system_time
 ) = false
 
-@test "configure: pure_color_system_time"  (
+@test "configure: pure_color_system_time" (
     set --erase pure_color_system_time
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_system_time
 ) = pure_color_mute
 
-@test "configure: pure_color_virtualenv"  (
+@test "configure: pure_color_virtualenv" (
     set --erase pure_color_virtualenv
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_virtualenv
 ) = pure_color_mute
 
-@test "configure: pure_begin_prompt_with_current_directory"  (
+@test "configure: pure_begin_prompt_with_current_directory" (
     set --erase pure_begin_prompt_with_current_directory
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_begin_prompt_with_current_directory
 ) = true
 
-@test "configure: pure_separate_prompt_on_error"  (
+@test "configure: pure_separate_prompt_on_error" (
     set --erase pure_separate_prompt_on_error
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_separate_prompt_on_error
 ) = false
 
-@test "configure: pure_threshold_command_duration"  (
+@test "configure: pure_threshold_command_duration" (
     set --erase pure_threshold_command_duration
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_threshold_command_duration
 ) = 5
 
-@test "configure: pure_show_subsecond_command_duration"  (
+@test "configure: pure_show_subsecond_command_duration" (
     set --erase pure_show_subsecond_command_duration
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_show_subsecond_command_duration
 ) = false
 
-@test "configure: pure_color_command_duration"  (
+@test "configure: pure_color_command_duration" (
     set --erase pure_color_command_duration
     source (dirname (status filename))/../conf.d/pure.fish
     echo $pure_color_command_duration
