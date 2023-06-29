6cf9442414081ecc3b9ab93324404625af64f47e
Édouard Lopez <contact@edouard-lopez.com>
2023-06-29 13:48:27 +0200

    fix: set default to false when detecting if we are inside container or not

--- functions/_pure_is_inside_container.fish
+++ functions/_pure_is_inside_container.fish
@@ -2,6 +2,7 @@ function _pure_is_inside_container \
     --argument-names cgroup_namespace
     set --query cgroup_namespace[1]; or set cgroup_namespace /proc/1/cgroup
 
+    set --local failure 1
     if set --query pure_enable_container_detection; and test "$pure_enable_container_detection" = true
         set --local success 0
         if test -n "$container"
@@ -19,8 +20,6 @@ function _pure_is_inside_container \
                 return $success
             end
         end
-
-        set --local failure 1
-        return $failure
     end
+    return $failure
 end
