d0d6eec495c6f22ef1ac5ad159eeae2a945654bc
Édouard Lopez <contact@edouard-lopez.com>
2023-08-11 11:26:57 +0200

    refactor: pass feature_flag as first argument of  _pure_check_availability

--- functions/_pure_prompt_k8s.fish
+++ functions/_pure_prompt_k8s.fish
@@ -1,5 +1,5 @@
 function _pure_prompt_k8s
-    if $pure_enable_k8s; and _pure_check_availability kubectl pure_enable_k8s
+    if $pure_enable_k8s; and _pure_check_availability pure_enable_k8s kubectl
         set -l context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
         set -l namespace (_pure_set_color $pure_color_k8s_namespace)(_pure_k8s_namespace)
         echo "$pure_symbol_k8s_prefix $context/$namespace"
