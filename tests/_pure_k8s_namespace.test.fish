7b9349654cb19c1c62da5078cdd26a0578089b2f
Édouard Lopez <contact@edouard-lopez.com>
2023-08-14 16:46:40 +0200

    test(k8s): add tests for _pure_k8s_namespace

---
+++ tests/_pure_k8s_namespace.test.fish
@@ -0,0 +1,27 @@
+source (dirname (status filename))/fixtures/constants.fish
+source (dirname (status filename))/../functions/_pure_k8s_namespace.fish
+@echo (_print_filename (status filename))
+
+
+function before_each
+    functions --erase head
+    _cleanup_calls
+end
+
+
+before_each
+@test "_pure_k8s_namespace: return context" (
+    function kubectl
+        echo my-namespace
+    end # mock
+
+    _pure_k8s_namespace
+) = my-namespace
+
+before_each
+@test "_pure_k8s_namespace: call `kubectl config view…`" (
+    function kubectl; echo (status function) $argv > /tmp/called; end # spy
+
+    _pure_k8s_namespace
+    _has_called "kubectl config view --minify --output jsonpath={..namespace}"
+) $status -eq $SUCCESS
