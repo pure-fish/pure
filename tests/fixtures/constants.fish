94de3b8dcbced31685ce55e76d17648ec00b7edd
Édouard Lopez <contact@edouard-lopez.com>
2023-08-30 11:11:40 +0200

    docs: comment why we check the $USER in some test

--- tests/fixtures/constants.fish
+++ tests/fixtures/constants.fish
@@ -9,7 +9,7 @@ set --global PURE_CONFIG_VERSION_REGEX '(?<=pure_version )(?<value>[^\s#]+)'
 set --global PURE_VERSION_NUMBER_REGEX '(\d+.\d+.\d+)$'
 
 function _purge_configs --description "Erase all existing pure configurations, to avoid unwanted side-effect"
-    if test "$USER" = nemo
+    if test "$USER" = nemo # avoid destroying other user's configuration
         for variable in (set --names | string match --regex --entire '^pure_')
             set --erase --local $variable
             set --erase --global $variable
@@ -19,7 +19,7 @@ function _purge_configs --description "Erase all existing pure configurations, t
 end
 
 function _disable_colors --description "Set all color to empty value, to avoid unwanted side-effect"
-    if test "$USER" = nemo
+    if test "$USER" = nemo # avoid destroying other user's configuration
         for color_config in (set --names | string match --regex --entire '^pure_color_')
             set --universal $color_config $EMPTY
         end
