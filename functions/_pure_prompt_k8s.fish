aba39343892bd4273dfbf16f4f2542aa1c779230
Bob Soppe <bobsoppe@users.noreply.github.com>
2023-09-22 14:33:44 +0200

    Use long form options

--- functions/_pure_prompt_k8s.fish
+++ functions/_pure_prompt_k8s.fish
@@ -4,12 +4,12 @@ function _pure_prompt_k8s
         and _pure_check_availability pure_enable_k8s kubectl
         and test -n "$(_pure_k8s_context)"
 
-        set -l symbol (_pure_set_color $pure_color_k8s_symbol)$pure_symbol_k8s_prefix
-        set -l context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
-        set -l namespace (_pure_set_color $pure_color_k8s_namespace)(_pure_k8s_namespace)
+        set --local symbol (_pure_set_color $pure_color_k8s_symbol)$pure_symbol_k8s_prefix
+        set --local context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
+        set --local namespace (_pure_set_color $pure_color_k8s_namespace)(_pure_k8s_namespace)
 
         if test -n "$namespace"
-            set -l namespace (_pure_set_color $pure_color_k8s_namespace)default
+            set --local namespace (_pure_set_color $pure_color_k8s_namespace)default
         end
 
         echo "$symbol $context/$namespace"
