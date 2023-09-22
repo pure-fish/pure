dbada87b5f540f77547cadd03a2776ae265ce876
Bob Soppe <bobsoppe@users.noreply.github.com>
2023-09-22 14:23:56 +0200

    Add logic to check if any k8s context is selected

--- functions/_pure_prompt_k8s.fish
+++ functions/_pure_prompt_k8s.fish
@@ -2,6 +2,7 @@ function _pure_prompt_k8s
     if set --query pure_enable_k8s;
         and test "$pure_enable_k8s" = true;
         and _pure_check_availability pure_enable_k8s kubectl
+        and test -n "$(_pure_k8s_context)"
 
         set -l symbol (_pure_set_color $pure_color_k8s_symbol)$pure_symbol_k8s_prefix
         set -l context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
