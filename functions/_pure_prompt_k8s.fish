87c8351542342b538a028deacfb8fade2c003cc6
Bob Soppe <bobsoppe@users.noreply.github.com>
2023-09-26 10:21:49 +0200

    Use fish 3.3.1 compatible syntax
    Co-authored-by: Édouard Lopez <edouard-lopez@users.noreply.github.com>

--- functions/_pure_prompt_k8s.fish
+++ functions/_pure_prompt_k8s.fish
@@ -2,7 +2,7 @@ function _pure_prompt_k8s
     if set --query pure_enable_k8s;
         and test "$pure_enable_k8s" = true;
         and _pure_check_availability pure_enable_k8s kubectl
-        and test -n "$(_pure_k8s_context)"
+        and test -n (_pure_k8s_context) # todo: use $(cmd) syntax when Fish 3.3.1 is dropped
 
         set --local symbol (_pure_set_color $pure_color_k8s_symbol)$pure_symbol_k8s_prefix
         set --local context (_pure_set_color $pure_color_k8s_context)(_pure_k8s_context)
