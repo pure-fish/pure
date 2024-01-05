5cec3916a60f964282c1587234fc08fa3a387fac
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-05 12:15:31 +0900

    refactor(nixdevshell): simplify prompt logic

--- functions/_pure_prompt_nixdevshell.fish
+++ functions/_pure_prompt_nixdevshell.fish
@@ -2,16 +2,11 @@ function _pure_prompt_nixdevshell \
     --description "Indicate if nix develop shell is activated"
 
     if set --query pure_enable_nixdevshell;
-        and test "$pure_enable_nixdevshell" = true
+        and test "$pure_enable_nixdevshell" = true;
+        and test -n "$IN_NIX_SHELL"
 
-        set --local nixdevshell ''
         set --local nixdevshell_color (_pure_set_color $pure_color_nixdevshell)
-        if test -n "$IN_NIX_SHELL"
-            set nixdevshell $IN_NIX_SHELL
-        end
 
-        if test -n $nixdevshell
-            echo "$pure_symbol_nixdevshell_prefix$nixdevshell_color$nixdevshell"
-        end
+        echo "$pure_symbol_nixdevshell_prefix$nixdevshell_color$IN_NIX_SHELL"
     end
 end
