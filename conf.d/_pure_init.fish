159e0b623f60ef4d23469e697505644909550434
Édouard Lopez <contact@edouard-lopez.com>
2024-12-19 23:51:07 +0100

    fix: source _pure_prompt_new_line event handler instead of relying on autoloading
    From the doc: https://fishshell.com/docs/current/language.html#event
    > Please note that event handlers only become active when a function is loaded,
    > which means you need to otherwise source or execute a function instead of
    > relying on autoloading.
    
    fix #356

--- conf.d/_pure_init.fish
+++ conf.d/_pure_init.fish
@@ -4,8 +4,8 @@ set --global --export VIRTUAL_ENV_DISABLE_PROMPT 1
 # Whether or not is a fresh session
 set --global _pure_fresh_session true
 
-# Register `_pure_prompt_new_line` as an event handler fot `fish_prompt`
-functions --query _pure_prompt_new_line
+# Register `_pure_prompt_new_line` as an event handler for `fish_prompt`
+source $__fish_config_dir/functions/_pure_prompt_new_line.fish
 
 function _pure_uninstall --on-event pure_uninstall
     rm -f $__fish_config_dir/conf.d/pure.fish
