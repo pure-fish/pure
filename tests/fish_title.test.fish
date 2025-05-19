8d365dbe60b795b8f91279fddff4e4d311607825
Édouard Lopez <contact@edouard-lopez.com>
2025-05-19 10:35:13 +0200

    test: wrap fish_title tests in condition for fish <4.0.0
    related #360

--- tests/fish_title.test.fish
+++ tests/fish_title.test.fish
@@ -1,5 +1,7 @@
 source (status dirname)/fixtures/constants.fish
 source (status dirname)/../functions/fish_title.fish
+source (status dirname)/../tools/versions-compare.fish
+
 @echo (_print_filename (status filename))
 
 
@@ -10,26 +12,28 @@ function before_all
 end
 before_all
 
-@test "fish_title: contains current directory and previous command" (
+if fish_version_below 4.0.0
+    @test "fish_title: contains current directory and previous command" (
     set --universal pure_symbol_title_bar_separator '-'
     fish_title 'last-command'
 ) = "/tmp/current/directory: last-command - fish"
 
-@test "fish_title: contains current directory with an *empty* previous command" (
+    @test "fish_title: contains current directory with an *empty* previous command" (
     fish_title ''
 ) = "/tmp/current/directory - fish"
 
-@test "fish_title: contains current path without a previous command" (
+    @test "fish_title: contains current path without a previous command" (
     set --universal pure_symbol_title_bar_separator '-'
     fish_title
 ) = "/tmp/current/directory - fish"
 
-@test "fish_title: contains shorten (1) current path" (
+    @test "fish_title: contains shorten (1) current path" (
     set --universal pure_shorten_window_title_current_directory_length 1
     fish_title
 ) = "/t/c/directory - fish"
 
-@test "fish_title: contains shorten (2) current path" (
+    @test "fish_title: contains shorten (2) current path" (
     set --universal pure_shorten_window_title_current_directory_length 2
     fish_title
 ) = "/tm/cu/directory - fish"
+end
