2577f3050c54fbc323ea7895c09b5cb6f77dcc39
Édouard Lopez <contact@edouard-lopez.com>
2025-05-19 18:08:06 +0200

    fix(fish_title): use index to ignore extraneous item in `status current-command`
    fixes #360
    waiting for a better solution in https://github.com/fish-shell/fish-shell/issues/11503

--- functions/fish_title.fish
+++ functions/fish_title.fish
@@ -3,7 +3,7 @@ function fish_title \
     --argument-names last_command
 
     set --local current_folder (fish_prompt_pwd_dir_length=$pure_shorten_window_title_current_directory_length prompt_pwd)
-    set --local current_command (status current-command 2>/dev/null; or echo $_)
+    set --local current_command (status current-command 2>/dev/null; or echo $_)[1] # we use index to ignore extraneous item, see #360
 
     set --local prompt "$current_folder: $last_command $pure_symbol_title_bar_separator $current_command"
 
