53d22810e51c43e431adc681a23a21f3114fd60d
Édouard Lopez <contact@edouard-lopez.com>
2025-09-13 20:37:48 +0200

    fix(vscode): prevent vscode from adding extra line
    Thanks to @wyw message
    https://github.com/microsoft/vscode/issues/187185#issuecomment-2694449635
    
    fixes #341 #379

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -5,7 +5,7 @@ function fish_prompt
     _pure_print_prompt_rows # manage default vs. compact prompt
     _pure_place_iterm2_prompt_mark # place iTerm shell integration mark
     echo -e -n (_pure_prompt $exit_code) # print prompt
-    echo -e (_pure_prompt_ending) # reset colors and end prompt
+    echo -e -n (_pure_prompt_ending) # reset colors and end prompt
 
     set _pure_fresh_session false
 end
