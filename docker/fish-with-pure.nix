228ce9392853bb1c0d87dca0110d985df12a4c5a
Édouard Lopez <contact@edouard-lopez.com>
2025-09-22 12:08:57 +0200

    ci(nix): add curl

--- docker/fish-with-pure.nix
+++ docker/fish-with-pure.nix
@@ -8,6 +8,7 @@ let
   localPure = (fishPlugins.pure.overrideAttrs (old: {
     src = builtins.fetchGit ./.;
     checkPlugins = [ fishPlugins.fishtape_3 ];
+    buildInputs = (old.buildInputs or []) ++ [ curl ];
 
     # We override after the `buildFishPlugin` wrapper applied its logic,
     # so we need to manually run stuff around what we see in the original plugin attr value.
