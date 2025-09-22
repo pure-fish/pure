0b26bd8db0a8ea7285191a298d2919f6ddc9c403
Kwee Lung Sin <klungs.dev@gmail.com>
2025-09-22 22:18:56 +0700

    Simplify three configs to one: pure_show_numbered_git_indicator

--- functions/_pure_prompt_git_stash.fish
+++ functions/_pure_prompt_git_stash.fish
@@ -10,7 +10,7 @@ function _pure_prompt_git_stash
     if test "$git_stash_count" -gt 0 # has git stash
         set git_stash_symbol " $pure_symbol_git_stash"
         set git_stash_color (_pure_set_color $pure_color_git_stash)
-        if test "$pure_show_numbered_git_stash" = true
+        if test "$pure_show_numbered_git_indicator" = true
             set git_stash_number "$git_stash_count"
         end
     end
--- tests/_pure.test.fish
+++ tests/_pure.test.fish
@@ -63,22 +63,10 @@ before_all
     echo $pure_symbol_git_stash
 ) = "≡"
 
-@test "configure: pure_show_numbered_git_unpulled_commits" (
-    set --erase pure_show_numbered_git_unpulled_commits
+@test "configure: pure_show_numbered_git_indicator" (
+    set --erase pure_show_numbered_git_indicator
     source (status dirname)/../conf.d/pure.fish
-    echo $pure_show_numbered_git_unpulled_commits
-) = false
-
-@test "configure: pure_show_numbered_git_unpushed_commits" (
-    set --erase pure_show_numbered_git_unpushed_commits
-    source (status dirname)/../conf.d/pure.fish
-    echo $pure_show_numbered_git_unpushed_commits
-) = false
-
-@test "configure: pure_show_numbered_git_stash" (
-    set --erase pure_show_numbered_git_stash
-    source (status dirname)/../conf.d/pure.fish
-    echo $pure_show_numbered_git_stash
+    echo $pure_show_numbered_git_indicator
 ) = false
 
 @test "configure: pure_symbol_title_bar_separator" (
