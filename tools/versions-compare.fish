41aafaf7b6d212f700b1901df8868047d2baffdb
Édouard Lopez <contact@edouard-lopez.com>
2025-11-27 15:12:04 +0100

    refactor: print message when skipping test based on version

--- tools/versions-compare.fish
+++ tools/versions-compare.fish
@@ -1,40 +1,57 @@
 source (status dirname)/../tests/fixtures/constants.fish
 
-
 function fish_version_below \
     --description "Compare versions.  By default this tests \$FISH_VERSION, but if a second argument is provided it tests against that version." \
-    --argument-names expected actual
+    --argument-names \
+    expected \
+    actual
 
-    if test -z "$actual"
-        set actual $FISH_VERSION
-    end
+    test -z "$actual" && set actual $FISH_VERSION
 
     if test "$expected" = $actual
         return $FAILURE
     end
 
     # busybox compatibility (see https://github.com/fish-shell/fish-shell/issues/4419#issuecomment-453512461)
-    printf '%s\n' $actual $expected | sort -c -t. -k 1,1n -k 2,2n -k 3,3n 2>/dev/null
+    printf '%s\n' $actual $expected \
+        | sort -c -t. -k 1,1n -k 2,2n -k 3,3n 2>/dev/null
 
     return $status
 end
 
 function fish_version_at_least \
     --description "Compare versions.  By default this tests \$FISH_VERSION, but if a second argument is provided it tests against that version." \
-    --argument-names expected actual
+    --argument-names \
+    expected \
+    actual
 
-    not fish_version_below $argv >/dev/null
+    function skip_message --argument-names expected
+        @echo (set_color yellow)"skip, "(set_color normal)(print_fish_version_at_least $expected)
+    end
 
-    return $status
+    if fish_version_below $expected $actual >/dev/null
+        skip_message $expected
+        return $FAILURE
+    end
+    return $SUCCESS
 end
 
 function print_fish_version_below \
-    --argument-names expected
+    --argument-names \
+    expected \
+    actual
 
-    printf "%sonly fish <%s: %s" (set_color blue) $expected (set_color normal) 1>&2
+    test -z "$actual" && set actual $FISH_VERSION
+
+    printf "requires Fish <%s, got %s%s%s" (set_color yellow) $expected (set_color red) $actual (set_color normal)
 end
 
 function print_fish_version_at_least \
-    --argument-names expected
-    printf "%sonly fish <%s: %s" (set_color blue) $expected (set_color normal) 1>&2
+    --argument-names \
+    expected \
+    actual
+
+    test -z "$actual" && set actual $FISH_VERSION
+
+    printf "requires Fish ≥%s, got %s%s%s" $expected (set_color red) $actual (set_color normal)
 end
