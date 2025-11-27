d40f1245c74c0ccb0cec43faf2c6887887c546cf
Édouard Lopez <contact@edouard-lopez.com>
2025-11-27 04:36:39 +0100

    refactor: drop migration script for pure 3.x -> 4.x

--- tests/fixtures/config.expected.fish
+++
@@ -1,6 +0,0 @@
-# FOR TESTS-ONLY
-_pure_set_default pure_color_hostname "value"
-_pure_set_default pure_color_at_sign "value"
-_pure_set_default pure_color_username_normal "value"
-_pure_set_default pure_color_username_root "value"
-# FOR TESTS-ONLY
