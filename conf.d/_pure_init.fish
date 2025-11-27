82478e4f348721f248ce9654f757f2d3f4379ab0
Édouard Lopez <contact@edouard-lopez.com>
2025-11-27 04:33:27 +0100

    test: support standalone build with no $__fish_data_dir directory
    https://github.com/fish-shell/fish-shell/issues/11429#issuecomment-2834407208

--- conf.d/_pure_init.fish
+++ conf.d/_pure_init.fish
@@ -5,14 +5,14 @@ set --global --export VIRTUAL_ENV_DISABLE_PROMPT 1
 set --global _pure_fresh_session true
 
 # Register `_pure_prompt_new_line` as an event handler for `fish_prompt`
-for fpath in $fish_function_path;
+for fpath in $fish_function_path
+
     test -e $fpath/_pure_prompt_new_line.fish && source $fpath/_pure_prompt_new_line.fish
 end
 
-
 function _pure_uninstall --on-event pure_uninstall \
     --description 'Fisher handler to uninstall pure prompt'
-    
+
     rm -f $__fish_config_dir/conf.d/pure.fish
 
     # backup fish_prompt and fish_title to default
@@ -22,13 +22,26 @@ function _pure_uninstall --on-event pure_uninstall \
     # erase existing fish_prompt and fish_title to default
     functions --erase fish_prompt
     functions --erase fish_title
-    # restore fish_prompt and fish_title to default
-    cp {$__fish_data_dir,$__fish_config_dir}/functions/fish_prompt.fish
-    cp {$__fish_data_dir,$__fish_config_dir}/functions/fish_title.fish
 
-    # refresh fish_prompt and fish_title definitions
-    source $__fish_data_dir/functions/fish_prompt.fish
-    source $__fish_data_dir/functions/fish_title.fish
+    # restore fish_prompt and fish_title to default
+    # reload them into active session
+    # To remain compatible with both single-binary fish and traditional installations
+    # https://github.com/fish-shell/fish-shell/issues/11429#issuecomment-2834407208
+    if status list-files functions/fish_prompt.fish >/dev/null # standalone binary Fish ≥4.1.2
+        status get-file functions/fish_prompt.fish \
+            | tee $__fish_config_dir/functions/fish_prompt.fish \
+            | source
+        status get-file functions/fish_title.fish \
+            | tee $__fish_config_dir/functions/fish_title.fish \
+            | source
+    else # traditional installation
+        cat $__fish_data_dir/functions/fish_prompt.fish \
+            | tee $__fish_config_dir/functions/fish_prompt.fish \
+            | source
+        cat $__fish_data_dir/functions/fish_title.fish \
+            | tee $__fish_config_dir/functions/fish_title.fish \
+            | source
+    end
 
     # erase _pure* variables
     set --names \
