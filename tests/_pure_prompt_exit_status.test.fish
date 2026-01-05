df5ce30d88f2f4992844c55236266b39d180a595
Maritsu <66056347+Maritsu@users.noreply.github.com>
2026-01-05 13:25:24 +0100

    test(_pure_prompt_exit_status): refactor tests to match suggested changes
    Refs: https://github.com/pure-fish/pure/pull/395
    
    test(_pure_prompt_exit_status): add expected output description to test titles
    
    test(_pure_prompt_exit_status): refactor signal conversion test to target the intended CI environment only

--- tests/_pure_prompt_exit_status.test.fish
+++ tests/_pure_prompt_exit_status.test.fish
@@ -10,7 +10,7 @@ function before_each
 end
 
 before_each
-@test "_pure_prompt_exit_status: single zero status, no output" (
+@test "_pure_prompt_exit_status: single zero status. no output" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $SUCCESS
     set --universal pure_color_exit_status red
@@ -18,7 +18,7 @@ before_each
 ) -z
 
 before_each
-@test "_pure_prompt_exit_status: single non-zero status" (
+@test "_pure_prompt_exit_status: single non-zero status. has output" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $FAILURE
     set --universal pure_color_exit_status red
@@ -26,7 +26,7 @@ before_each
 ) = (set_color red)"$FAILURE"
 
 before_each
-@test "_pure_prompt_exit_status: pipeline with all zero statuses, no output" (
+@test "_pure_prompt_exit_status: pipeline with all zero statuses. no output" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $SUCCESS $SUCCESS $SUCCESS $SUCCESS
     set --universal pure_color_exit_status red
@@ -34,7 +34,7 @@ before_each
 ) -z
 
 before_each
-@test "_pure_prompt_exit_status: pipeline with non-zero statuses, last status is zero, no output" (
+@test "_pure_prompt_exit_status: pipeline with non-zero statuses, last status is zero. no output" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $SUCCESS $FAILURE $SUCCESS $SUCCESS
     set --universal pure_color_exit_status red
@@ -42,7 +42,7 @@ before_each
 ) -z
 
 before_each
-@test "_pure_prompt_exit_status: pipeline with non-zero statuses, last status is non-zero" (
+@test "_pure_prompt_exit_status: pipeline with non-zero statuses, last status is non-zero. shows all statuses" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $SUCCESS $FAILURE $SUCCESS $FAILURE
     set --universal pure_color_exit_status red
@@ -51,7 +51,7 @@ before_each
 ) = (set_color red)"$SUCCESS|$FAILURE|$SUCCESS|$FAILURE"
 
 before_each
-@test "_pure_prompt_exit_status: single signal and conversion disabled" (
+@test "_pure_prompt_exit_status: single signal and conversion disabled. shows status as number" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses 139
     set --universal pure_color_exit_status red
@@ -61,7 +61,7 @@ before_each
 ) = (set_color red)"139"
 
 before_each
-@test "_pure_prompt_exit_status: pipeline with a mix of everything, conversion disabled, last status is a signal" (
+@test "_pure_prompt_exit_status: pipeline with a mix of everything, conversion disabled, last status is a signal. shows all as numbers" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $SUCCESS $FAILURE 123 139 $FAILURE $SUCCESS 54 141 $FAILURE 137
     set --universal pure_color_exit_status red
@@ -71,33 +71,35 @@ before_each
 ) = (set_color red)"0|1|123|139|1|0|54|141|1|137"
 
 before_each
-@test "_pure_prompt_exit_status: pipeline with a mix of everything, conversion enabled, last status is a signal" (
+@test "_pure_prompt_exit_status: pipeline with a mix of everything, conversion enabled, last status is a signal. shows mix of numbers and signal names" (
     _pure_unmock _pure_set_color # enable colors
     set --local statuses $SUCCESS $FAILURE 123 139 $FAILURE $SUCCESS 54 141 $FAILURE 137
     set --universal pure_color_exit_status red
     set --universal pure_symbol_exit_status_separator '|'
     set --universal pure_convert_exit_status_to_signal true
     _pure_prompt_exit_status $statuses
-) = (set_color red)"0|1|123|SIGSEGV|1|0|54|SIGPIPE|1|SIGKILL"
+) = (set_color red)"0|1|123|SIGSEGV|1|0|54|SIGPIPE|1|SIGKILL" # NOTE: AFAIK these three are the same across all OSes, so this test shouldn't fail
 
 before_each
-@test "_pure_prompt_exit_status: pipeline of the first 15 signals, conversion enabled" (
-    _pure_unmock _pure_set_color # enable colors
-    set --local statuses (seq 129 143)
-    set --universal pure_color_exit_status red
-    set --universal pure_symbol_exit_status_separator '|'
-    set --universal pure_convert_exit_status_to_signal true
-    _pure_prompt_exit_status $statuses
-) = (set_color red)(
-    # NOTE: this is required for the test to pass when not running on Linux,
-    # since different kernels can have different signal meanings
-    set expected ""
-    for code in (seq 129 143)
-        if test -z "$expected"
-            set expected (fish_status_to_signal $code)
-        else
-            set expected (string join "|" "$expected" (fish_status_to_signal $code))
-        end
-    end
-    echo $expected
-)
+if test (command uname -s) != "Darwin" # non-macOS
+    @test "_pure_prompt_exit_status: pipeline of the first 15 signals, conversion enabled. shows all as (non-macOS) signal names" (
+        _pure_unmock _pure_set_color # enable colors
+        set --local statuses (seq 129 143)
+        set --universal pure_color_exit_status red
+        set --universal pure_symbol_exit_status_separator '|'
+        set --universal pure_convert_exit_status_to_signal true
+        _pure_prompt_exit_status $statuses
+    ) = (set_color red)"SIGHUP|SIGINT|SIGQUIT|SIGILL|SIGTRAP|SIGABRT|SIGBUS|SIGFPE|SIGKILL|SIGUSR1|SIGSEGV|SIGUSR2|SIGPIPE|SIGALRM|SIGTERM"
+end
+
+before_each
+if test (command uname -s) = "Darwin" # macOS
+    @test "_pure_prompt_exit_status: pipeline of the first 15 signals, conversion enabled. shows all as (macOS-specific) signal names" (
+        _pure_unmock _pure_set_color # enable colors
+        set --local statuses (seq 129 143)
+        set --universal pure_color_exit_status red
+        set --universal pure_symbol_exit_status_separator '|'
+        set --universal pure_convert_exit_status_to_signal true
+        _pure_prompt_exit_status $statuses
+    ) = (set_color red)"SIGHUP|SIGINT|SIGQUIT|SIGILL|SIGTRAP|SIGABRT|SIGEMT|SIGFPE|SIGKILL|SIGBUS|SIGSEGV|SIGSYS|SIGPIPE|SIGALRM|SIGTERM"
+end
