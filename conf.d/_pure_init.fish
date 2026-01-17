57f5f357c5340ed2e4aeeab6bd29ab4c0e6706d5
Benedict Lee <benedictleejh@gmail.com>
2026-01-17 23:32:05 +0800

    feat: support installation locations not in fish config dir

--- conf.d/_pure_init.fish
+++ conf.d/_pure_init.fish
@@ -4,6 +4,8 @@ set --global --export VIRTUAL_ENV_DISABLE_PROMPT 1
 # Whether or not is a fresh session
 set --global _pure_fresh_session true
 
+set --global _pure_root (status current-filename | string replace --regex '/[^/]+$' '' | string replace --regex '/[^/]+$' '')
+
 # Register `_pure_prompt_new_line` as an event handler for `fish_prompt`
 for fpath in $fish_function_path
     test -e $fpath/_pure_prompt_new_line.fish && source $fpath/_pure_prompt_new_line.fish
@@ -19,22 +21,22 @@ end
 
 function _pure_install --on-event pure_install \
     --description 'Fisher handler when pure prompt is installed'
-    
-    source $__fish_config_dir/conf.d/pure.fish
-    
+
+    source $_pure_root/conf.d/pure.fish
+
     printf "Now using: %s %s\n" \
         (_pure) \
         (set_color --bold $pure_color_success)$pure_version(set_color normal)
 end
 
-# Fisher emit an event for all files under conf.d/. 
+# Fisher emit an event for all files under conf.d/.
 # We listen to the event when the new conf.d/_pure_init.fish is sourced,
 # to read old `pure_version` before sourcing conf.d/pure.fish happens.
 function _pure_update --on-event _pure_init_update \
     --description 'Fisher handler pure prompt is updated'
-    
+
     set --local previous_version $pure_version
-    source $__fish_config_dir/conf.d/pure.fish
+    source $_pure_root/conf.d/pure.fish
 
     printf "Updating: %s %s → %s\n" \
         (_pure) \
@@ -45,11 +47,11 @@ end
 function _pure_uninstall --on-event pure_uninstall \
     --description 'Fisher handler pure prompt is uninstalled'
 
-    rm -f $__fish_config_dir/conf.d/pure.fish
+    rm -f $_pure_root/conf.d/pure.fish
 
     # backup fish_prompt and fish_title to default
-    cp $__fish_config_dir/functions/fish_prompt{,.pure-backup}.fish
-    cp $__fish_config_dir/functions/fish_title{,.pure-backup}.fish
+    cp $_pure_root/functions/fish_prompt{,.pure-backup}.fish
+    cp $_pure_root/functions/fish_title{,.pure-backup}.fish
 
     # erase existing fish_prompt and fish_title to default
     functions --erase fish_prompt
@@ -61,17 +63,17 @@ function _pure_uninstall --on-event pure_uninstall \
     # https://github.com/fish-shell/fish-shell/issues/11429#issuecomment-2834407208
     if status list-files functions/fish_prompt.fish >/dev/null # standalone binary Fish ≥4.1.2
         status get-file functions/fish_prompt.fish \
-            | tee $__fish_config_dir/functions/fish_prompt.fish \
+            | tee $_pure_root/functions/fish_prompt.fish \
             | source
         status get-file functions/fish_title.fish \
-            | tee $__fish_config_dir/functions/fish_title.fish \
+            | tee $_pure_root/functions/fish_title.fish \
             | source
     else # traditional installation
         cat $__fish_data_dir/functions/fish_prompt.fish \
-            | tee $__fish_config_dir/functions/fish_prompt.fish \
+            | tee $_pure_root/functions/fish_prompt.fish \
             | source
         cat $__fish_data_dir/functions/fish_title.fish \
-            | tee $__fish_config_dir/functions/fish_title.fish \
+            | tee $_pure_root/functions/fish_title.fish \
             | source
     end
 
@@ -84,7 +86,7 @@ function _pure_uninstall --on-event pure_uninstall \
         | string replace --filter --regex '(^_?pure)' 'functions --erase $1' \
         | source
     # delete _pure* files
-    for file in $__fish_config_dir/{functions,conf.d}/_pure_*
+    for file in $_pure_root/{functions,conf.d}/_pure_*
         rm -f $file
     end
 end
