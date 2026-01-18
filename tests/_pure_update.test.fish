d9354c2a6e3a54214ec735df06678665a4008842
Benedict Lee <benedictleejh@gmail.com>
2026-01-18 16:49:14 +0800

    test: update tests to handle custom install dir support

--- tests/_pure_update.test.fish
+++ tests/_pure_update.test.fish
@@ -10,11 +10,12 @@ before_all
 function before_each
     functions --erase _pure_update
     mkdir -p $__fish_config_dir/conf.d
+    cp (status dirname)/../conf.d/_pure_init.fish $__fish_config_dir/conf.d/
 end
 
 before_each
 @test "init/_pure_update: handler is available" (
-    source (status dirname)/../conf.d/_pure_init.fish
+    source $__fish_config_dir/conf.d/_pure_init.fish
 
     functions --query _pure_update
 ) $status -eq $SUCCESS
@@ -24,7 +25,7 @@ before_each
     set --global pure_symbol_prompt "❯"
     set --global pure_symbol_reverse_prompt "❮"
     set --global pure_version 1.0.0 # current version
-    source (status dirname)/../conf.d/_pure_init.fish
+    source $__fish_config_dir/conf.d/_pure_init.fish
     _mock_response set_color $EMPTY
 
     # _pure_update reads the version from conf.d/pure.fish
@@ -36,7 +37,7 @@ before_each
 before_each
 @test "init/_pure_update: `_pure_update` run when `_pure_init_update` event is emitted" (
     set --global pure_version 1.0.0 # current version
-    source (status dirname)/../conf.d/_pure_init.fish
+    source $__fish_config_dir/conf.d/_pure_init.fish
     echo "set --global pure_version 2.0.0 # new version" > $__fish_config_dir/conf.d/pure.fish
 
     emit _pure_init_update # due to this event, test output will display: Updating: ❯❮❯ pure 1.0.0 → 2.0.0
