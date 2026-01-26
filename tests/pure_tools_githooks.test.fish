961ecf40b7282ac20f3fdfcdcd20d4daccb48cdf
Édouard Lopez <contact@edouard-lopez.com>
2026-01-26 18:12:53 +0100

    chore(hook): skip hook when switching to `master` branch

--- tests/pure_tools_githooks.test.fish
+++ tests/pure_tools_githooks.test.fish
@@ -1,4 +1,6 @@
+source (status dirname)/../tools/versions-compare.fish
 source (status dirname)/../tools/githooks.fish
+@echo (_print_filename (status filename))
 
 if fish_version_at_least 3.4.0 # --groups-only requires https://github.com/fish-shell/fish-shell/releases/tag/3.4.0
 @test "_pure_tools_githooks_sanitize_branch: replaces slash with hyphen" (
@@ -26,4 +28,27 @@ if fish_version_at_least 3.4.0 # --groups-only requires https://github.com/fish-
     set branch 'feature/foo'
     _pure_tools_githooks_get_new_version_line "$line" "$branch"
 ) = '# just a comment'
+
+@test "pure_tools_githooks_update_version_file: updates version in actual file" (
+    set temp_file (mktemp)
+    echo 'set --global pure_version 2.5.1 # For bug report' > $temp_file
+    echo '# Base colors' >> $temp_file
+    
+    pure_tools_githooks_update_version_file "$temp_file" 'feature/awesome'
+    
+    set result (grep 'pure_version' $temp_file)
+    rm -f $temp_file
+    
+    echo "$result"
+) = 'set --global pure_version 2.5.1-feature-awesome # For bug report'
+
+@test "pure_tools_githooks_update_version_file: handles non-existent file gracefully" (
+    pure_tools_githooks_update_version_file '/tmp/nonexistent-file-12345.fish' 'some-branch' 2>&1 | grep -q 'Error: File not found'
+) $status -eq 0
+
+@test "_pure_tools_githooks_get_new_version_line: works with master branch" (
+    set line 'set --global pure_version 1.2.3 # comment'
+    set branch 'master'
+    _pure_tools_githooks_get_new_version_line "$line" "$branch"
+) = 'set --global pure_version 1.2.3-master # comment'
 end
