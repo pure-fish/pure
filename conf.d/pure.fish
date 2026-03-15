a5121b7963e501746dd8b3baeff04de6251299e9
Kainoa Kanter <kainoa@t1c.dev>
2026-03-15 18:07:43 -0400

    feat: `pure_12hr_system_time` option for 12 hour system time

--- conf.d/pure.fish
+++ conf.d/pure.fish
@@ -47,6 +47,7 @@ _pure_set_default pure_color_jobs pure_color_normal
 
 # Show system time
 _pure_set_default pure_show_system_time false
+_pure_set_default pure_12hr_system_time false
 _pure_set_default pure_color_system_time pure_color_mute
 
 # Nix build environment
--- functions/_pure_prompt_system_time.fish
+++ functions/_pure_prompt_system_time.fish
@@ -1,6 +1,10 @@
 function _pure_prompt_system_time --description "Display system time"
     if set --query pure_show_system_time; and test "$pure_show_system_time" = true
         set --local time_color (_pure_set_color $pure_color_system_time)
-        echo "$time_color"(date '+%T')
+        if set --query pure_12hr_system_time; and test "$pure_12hr_system_time" = true
+            echo "$time_color"(date '+%I:%M:%S %p')
+        else
+            echo "$time_color"(date '+%T')
+        end
     end
 end
