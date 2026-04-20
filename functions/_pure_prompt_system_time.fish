dd99c7c94abf681613450649c26a19563b7664d6
Kainoa Kanter <kainoa@t1c.dev>
2026-04-20 14:15:33 -0700

    Update functions/_pure_prompt_system_time.fish
    Co-authored-by: Édouard Lopez <edouard-lopez@users.noreply.github.com>

--- functions/_pure_prompt_system_time.fish
+++ functions/_pure_prompt_system_time.fish
@@ -1,10 +1,6 @@
 function _pure_prompt_system_time --description "Display system time"
     if set --query pure_show_system_time; and test "$pure_show_system_time" = true
         set --local time_color (_pure_set_color $pure_color_system_time)
-        if set --query pure_system_time_format
-            echo "$time_color"(date $pure_system_time_format)
-        else
-            echo "$time_color"(date '+%T')
-        end
+        echo "$time_color"(date $pure_system_time_format)
     end
 end
